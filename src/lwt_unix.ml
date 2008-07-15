4d7d5ccb209afec91e1b480b489e2296b30f0035
Stephane Glondu <steph@glondu.net>
2008-07-15 14:41:39 +0200

    Precisions in source headers
    darcs-hash:20080715124139-0445d-4fc7693f8fadcad8a7701b3c53c1a75b59f6c769

--- src/lwt_unix.ml
+++ src/lwt_unix.ml
@@ -6,9 +6,9 @@
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, with linking exception;
+ * published by the Free Software Foundation, with linking exceptions;
  * either version 2.1 of the License, or (at your option) any later
- * version.
+ * version. See COPYING file for details.
  *
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
