5e7b7b71604b5cf8dcbb426388425fe6d34dfe5c
Jeremie Dimino <jeremie@dimino.org>
2009-04-12 13:24:53 +0200

    better representation of threads
    Ignore-this: cc3c9e7b4084d43f029f2d9ed09f3be5
    
    [waiters = CNil] iff [state = Sleep].
    
    darcs-hash:20090412112453-c41ad-3e99da8d2ad5da6a9b21fa5759f8393df94e27a5

--- src/lwt.ml
+++ src/lwt.ml
@@ -37,181 +37,173 @@ let capp l l' =
   | _, CNil -> l
   | _       -> CApp (l, l')
 
-let rec citer_rec f l rem =
+let rec citer_rec x l rem =
   match l, rem with
     CNil, [] ->
       ()
   | CNil, l :: rem ->
-      citer_rec f l rem
-  | CSingle v, [] ->
-      f v
-  | CSingle v, l :: rem ->
-      f v;
-      citer_rec f l rem
+      citer_rec x l rem
+  | CSingle f, [] ->
+      f x
+  | CSingle f, l :: rem ->
+      f x;
+      citer_rec x l rem
   | CApp (l, l'), _ ->
-      citer_rec f l (l' :: rem)
+      citer_rec x l (l' :: rem)
 
-let citer f l = citer_rec f l []
+let citer x l = citer_rec x l []
 
 (* Either a thread ['a t] has terminated, either successfully [Return of 'a] or
  *  unsuccessfully [Fail of exn], or it is sleeping, or it behaves the same
  *  as some representant.
+ * A sleeping thread could have several [waiters], which are thunk functions.
  *)
 type 'a state =
     Return of 'a
   | Fail of exn
-  | Sleep
+  | Sleep of ('a t -> unit) clist
   | Repr of 'a t  (* implements union-find *)
 
-(* A suspended thread is described by ['a t]
- * It could have several [waiters], which are thunk functions
- *)
-and 'a t =
-  { mutable state : 'a state;
-    mutable waiters : ('a t -> unit) clist }
-
-(* [make st] returns a thread of state [st] and no waiters *)
-let make st = { state = st; waiters = CNil }
-
-(* add a thunk [f] to the waiting list of thread [t]
-   [f] will be called with an argument that is either [Fail ...] or
-   [Return ...]. *)
-let add_waiter t f = t.waiters <- ccons f t.waiters
+and 'a t = 'a state ref
 
 let rec repr t =
-  match t.state with
-    Repr t' -> let t'' = repr t' in if t'' != t' then t.state <- Repr t''; t''
-  | _       -> t
+  match !t with
+    | Repr t' -> let t'' = repr t' in if t'' != t' then t := Repr t''; t''
+    | _       -> t
 
 (* restart a sleeping thread [t], run all its waiters
  * and running all the waiters, and make the terminating state [st]
  * [caller] is a string that describes the caller
  *)
 let restart t st caller =
-  assert (st <> Sleep);
   let t = repr t in
-  if t.state <> Sleep then invalid_arg caller;
-  t.state <- st;
-  let waiters = t.waiters in
-  t.waiters <- CNil;
-  citer (fun f -> f t) waiters
+  match !t with
+    | Sleep waiters ->
+        t := st;
+        citer t waiters
+    | _ ->
+        invalid_arg caller
 
 (*
- * pre-condition: [(repr t).state] is Sleep (i.e., not terminated)
+ * pre-condition: [!t] is [Sleep _] (i.e., not terminated)
  * [connect t t'] connects the two processes when t' finishes up
  * connecting means: running all the waiters for [t']
  * and assigning the state of [t'] to [t]
  *)
 let rec connect t t' =
-  let t = repr t in
-  let t' = repr t' in
-  if t.state <> Sleep then invalid_arg "connect";
-  if t == t' then
-    ()
-  else if t'.state = Sleep then begin
-    t'.state <- Repr t;
-    t.waiters <- capp t'.waiters t.waiters;
-    t'.waiters <- CNil
-  end else begin
-    t.state <- t'.state;
-    let waiters = t.waiters in
-    t.waiters <- CNil;
-    citer (fun f -> f t) waiters
-  end
+  let t = repr t and t' = repr t' in
+  match !t with
+    | Sleep waiters ->
+        if t == t' then
+          ()
+        else begin
+          match !t' with
+            | Sleep waiters' ->
+                t' := Repr t;
+                t := Sleep(capp waiters waiters')
+            | state' ->
+                t := state';
+                citer t waiters
+        end
+    | _ ->
+        invalid_arg "connect"
 
 (* similar to [connect t t']; does nothing instead of raising exception when
  * [t] is not asleep
  *)
 let rec try_connect t t' =
-  let t = repr t in
-  let t' = repr t' in
-  if t == t' || t.state <> Sleep then
-    ()
-  else if t'.state = Sleep then
-    add_waiter t' (fun t' -> try_connect t t')
-  else begin
-    t.state <- t'.state;
-    let waiters = t.waiters in
-    t.waiters <- CNil;
-    citer (fun f -> f t) waiters
-  end
+  let t = repr t and t' = repr t' in
+  match !t with
+    | Sleep waiters ->
+        if t == t' then
+          ()
+        else begin
+          match !t' with
+            | Sleep waiters' ->
+                t' := Sleep(ccons (fun t' -> try_connect t t') waiters')
+            | state' ->
+                t := state';
+                citer t waiters
+        end
+    | _ ->
+        ()
 
 (* apply function, reifying explicit exceptions into the thread type
  * apply: ('a -(exn)-> 'b t) -> ('a -(n)-> 'b t)
  * semantically a natural transformation TE -> T, where T is the thread
  * monad, which is layered over exception monad E.
  *)
-let apply f x = try f x with e -> make (Fail e)
+let apply f x = try f x with e -> ref (Fail e)
 
 (****)
 
-let return v = make (Return v)
-let fail e = make (Fail e)
+let return v = ref (Return v)
+let fail e = ref (Fail e)
 
-let wait () = make Sleep
+let wait () = ref (Sleep CNil)
 let wakeup t v = restart t (Return v) "wakeup"
 let wakeup_exn t e = restart t (Fail e) "wakeup_exn"
 
-let rec bind x f =
-  match (repr x).state with
-    Return v ->
-      (* we don't use apply here so that tail recursion is not broken *)
-      f v
-  | Fail e ->
-      fail e
-  | Sleep ->
-      let res = wait () in
-      add_waiter x (fun x -> connect res (bind x (apply f)));
-      res
-  | Repr _ ->
-      assert false
+let rec bind t f =
+  match !(repr t) with
+    | Return v ->
+        (* we don't use apply here so that tail recursion is not broken *)
+        f v
+    | Fail e ->
+        fail e
+    | Sleep waiters ->
+        let res = wait () in
+        t := Sleep(ccons (fun x -> connect res (bind x (apply f))) waiters);
+        res
+    | Repr _ ->
+        assert false
 let (>>=) = bind
 
-let rec catch_rec x f =
-  match (repr x).state with
-    Return v ->
-      x
-  | Fail e ->
-      f e
-  | Sleep ->
-      let res = wait () in
-      add_waiter x (fun x -> connect res (catch_rec x (apply f)));
-      res
-  | Repr _ ->
-      assert false
+let rec catch_rec t f =
+  match !(repr t) with
+    | Return v ->
+        t
+    | Fail e ->
+        f e
+    | Sleep waiters ->
+        let res = wait () in
+        t := Sleep(ccons (fun x -> connect res (catch_rec x (apply f))) waiters);
+        res
+    | Repr _ ->
+        assert false
 
 let catch x f = catch_rec (apply x ()) f
 
-let rec try_bind_rec x f g =
-  match (repr x).state with
-    Return v ->
-      f v
-  | Fail e ->
-      apply g e
-  | Sleep ->
-      let res = wait () in
-      add_waiter x (fun x -> connect res (try_bind_rec x (apply f) g));
-      res
-  | Repr _ ->
-      assert false
+let rec try_bind_rec t f g =
+  match !(repr t) with
+    | Return v ->
+        f v
+    | Fail e ->
+        apply g e
+    | Sleep waiters ->
+        let res = wait () in
+        t := Sleep(ccons (fun x -> connect res (try_bind_rec x (apply f) g)) waiters);
+        res
+    | Repr _ ->
+        assert false
 
 let try_bind x f = try_bind_rec (apply x ()) f
 
-let poll x =
-  match (repr x).state with
+let poll t =
+  match !(repr t) with
     Fail e   -> raise e
   | Return v -> Some v
-  | Sleep    -> None
+  | Sleep _  -> None
   | Repr _   -> assert false
 
-let rec ignore_result x =
-  match (repr x).state with
+let rec ignore_result t =
+  match !(repr t) with
     Return v ->
       ()
   | Fail e ->
       raise e
-  | Sleep ->
-      add_waiter x (fun x -> ignore_result x)
+  | Sleep waiters ->
+      t := Sleep(ccons (fun x -> ignore_result x) waiters)
   | Repr _ ->
       assert false
 
@@ -221,18 +213,18 @@ let rec nth_ready l n =
       assert false
   | x :: rem ->
       let x = repr x in
-      if x.state = Sleep then
-        nth_ready rem n
-      else if n > 0 then
-        nth_ready rem (n - 1)
-      else
-        x
+      match !x with
+        | Sleep _ ->
+            nth_ready rem n
+        | _ when n > 0 ->
+            nth_ready rem (n - 1)
+        | _ ->
+            x
 
 let choose l =
-  let ready = ref 0 in
-  List.iter (fun x -> if (repr x).state <> Sleep then incr ready) l;
-  if !ready > 0 then
-    nth_ready l (Random.int !ready)
+  let ready = List.fold_left (fun acc x -> match !x with Sleep _ -> acc | _ -> acc + 1) 0 l in
+  if ready > 0 then
+    nth_ready l (Random.int ready)
   else
     let res = wait () in
     (* XXX We may leak memory here, if we repeatedly select the same event *)
