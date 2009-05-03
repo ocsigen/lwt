3b1d2a31a7e34a0a13897008975cb6b80101720a
Jeremie Dimino <jeremie@dimino.org>
2009-05-03 10:47:03 +0200

    new module Lwt_printf
    Ignore-this: 7a6aa9fb4dfee8fa2af2b468f8943903
    
    darcs-hash:20090503084703-c41ad-02828ba0a69f87344d12a986ae494ee28087ba99

---
+++ src/lwt_printf.ml
@@ -0,0 +1,40 @@
+(* Lightweight thread library for Objective Caml
+ * http://www.ocsigen.org/lwt
+ * Module Lwt_printf
+ * Copyright (C) 2009 Jérémie Dimino
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, with linking exceptions;
+ * either version 2.1 of the License, or (at your option) any later
+ * version. See COPYING file for details.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ *)
+
+open Lwt
+open Lwt_io
+
+let fprintf oc fmt =
+  Printf.ksprintf (fun str -> put_string oc (Text.encode ~fallback:"?" str)) fmt
+
+let fprintln oc fmt =
+  Printf.ksprintf (fun str ->
+                     atomic
+                       (fun oc ->
+                          put_string oc (Text.encode ~fallback:"?" str)
+                          >> put_string oc (Text.encode (if Lwt_term.raw_mode () then "\r\n" else "\n")))
+                       oc) fmt
+
+let printf fmt = fprintf stdout fmt
+let println fmt = fprintln stdout fmt
+let eprintf fmt = fprintf stderr fmt
+let eprintln fmt = fprintln stderr fmt
