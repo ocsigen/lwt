0b16339eb33095d35fc5dde4747f645759ce7bfd
Jeremie Dimino <jeremie@dimino.org>
2009-05-10 20:55:18 +0200

    new example parallelize
    Ignore-this: 7101a5830df8767071e2af2c1fe7f4a4
    
    darcs-hash:20090510185518-c41ad-8aaac08dfa10bf78b13a37417f766e3426b3e6c1

---
+++ examples/parallelize.ml
@@ -0,0 +1,40 @@
+(*
+ * parallelize.ml
+ * --------------
+ * Copyright : (c) 2009, Jeremie Dimino <jeremie@dimino.org>
+ * Licence   : BSD3
+ *)
+
+(* Reads commands from standard input and launch them in parallel,
+   using as many processes as the number of CPUs. *)
+
+open Lwt_pervasives
+open Lwt_process
+
+(* Reads one command, launch it and waits for when it termination,
+   then start again: *)
+let rec launch () =
+  read_line_opt stdin >>= function
+    | None ->
+        return ()
+    | Some line ->
+        exec (shell line) >> launch ()
+
+(* Creates the initial <N> threads, where <N> is the number of
+   CPUs: *)
+let rec create_threads = function
+  | 0 ->
+      return ()
+  | n ->
+      launch () <&> create_threads (n - 1)
+
+(* Counts the number of CPUs using "/proc/cpuinfo": *)
+let cpus_count () =
+  Lwt_stream.fold (fun _ n -> succ n)
+    (Lwt_stream.filter
+       (fun line -> match Text.words line with
+          | "processor" :: _ -> true
+          | _ -> false)
+       (lines_of_file "/proc/cpuinfo")) 0
+
+let _ = Lwt_main.run (cpus_count () >>= create_threads)
