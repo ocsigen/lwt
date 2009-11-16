d3918b1021c5635b5575c0e96b09a0e3bfb1e9a0
Jeremie Dimino <jeremie@dimino.org>
2009-11-16 09:26:50 +0100

    add comments in examples
    Ignore-this: a00574f196a69e371cb1b4b3091728f9
    
    darcs-hash:20091116082650-c41ad-0fc13c38d73ebaf37d6af061228812951008a8b6

--- examples/kikoolol_prompt.ml
+++ examples/kikoolol_prompt.ml
@@ -1,27 +1,34 @@
+
+(* This example show how to fully customize read-line *)
+
 open Lwt
 open Lwt_term
 open Lwt_read_line
 
-let time, set_time = React.S.create ~eq:(fun _ _ -> false) ()
-  (* Signal changing every seconds *)
+(* Signal changing every seconds *)
+let time =
+  let time, set_time = React.S.create ~eq:(fun _ _ -> false) () in
 
-(* Update time every seconds *)
-let rec update_time () =
-  set_time ();
-  lwt () = Lwt_unix.sleep 1.0 in
-  update_time ()
-in
-update_time ()
+  (* Update time every seconds *)
+  let rec update_time () =
+    set_time ();
+    lwt () = Lwt_unix.sleep 1.0 in
+    update_time ()
+  in
+  ignore (update_time ());
 
+  time
+
+(* Customized read-line class *)
 class kikoolol =
-  let prompt = ref (React.S.const []) in
 object(self)
   inherit read_line ~prompt:[] ()
 
-  method prompt = !prompt
+  val mutable prompt = React.S.const []
+  method prompt = prompt
 
   initializer
-    prompt := React.S.l3 begin fun columns engine_state () ->
+    prompt <- React.S.l4 begin fun columns engine_state clipboard () ->
       let tm = Unix.localtime (Unix.time ()) in
       let columns = React.S.value Lwt_term.columns in
       [Bold;
@@ -32,14 +39,16 @@ object(self)
                                                      | Engine.Selection _ -> "selection"
                                                      | Engine.Edition _ -> "edition"
                                                      | Engine.Search _ -> "searching"); Text "\n";
-       Bold; fg lwhite; Text "clipboard: "; Reset; Text(React.S.value clipboard#contents); Text "\n";
+       Bold; fg lwhite; Text "clipboard: "; Reset; Text clipboard; Text "\n";
        Bold; fg lwhite; Text "length: "; Reset; Text(string_of_int (Text.length (Engine.all_input engine_state))); Text "\n";
        Bold;
        fg lred; Text(try Sys.getenv "USER" with Not_found -> "");
        fg lgreen; Text "@";
        fg lblue; Text(Unix.gethostname ());
        fg lgreen; Text " $ "]
-    end Lwt_term.columns self#engine_state time
+    end Lwt_term.columns self#engine_state self#clipboard#contents time;
+
+    self#set_message (Some "No completion.")
 end
 
 let () = Lwt_main.run begin
