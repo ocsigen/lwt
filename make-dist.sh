2b270cd8b0b797e6a2e5d78ce9d237e72c0a0f78
Jeremie Dimino <jeremie@dimino.org>
2010-10-23 01:00:47 +0200

    update oasis stuff to oasis 0.2
    Ignore-this: bd524850f8f4b4aa10bbfd9077e6120b
    
    darcs-hash:20101022230047-c41ad-10e5f2906114f64e7aa60bbb1a0fd88715c86933

--- make-dist.sh
+++ make-dist.sh
@@ -3,7 +3,7 @@
 DARCS_REPO=`pwd`
 export DARCS_REPO
 
-NAME=`awk '$1 == "Name:" {print $2}' _oasis`
-VERSION=`awk '$1 == "Version:" {print $2}' _oasis`
+NAME=`oasis query Name | tail -n 1`
+VERSION=`oasis query Version | tail -n 1`
 
 darcs dist --dist-name $NAME-$VERSION
