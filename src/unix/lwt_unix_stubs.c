9fe4e4b7201f659b6741f0190b6eb7cfcaa5b267
Thomas Leonard <talex5@gmail.com>
2014-05-04 17:54:53 +0100

    Don't throw an exception in the notification handler if we're shutting down
    OCaml calls WSACleanup in an at_exit handler. If we try to send a
    notification after that we get an error and throw an exception in the
    signal handler. This causes a segfault.
    
    Fixes #59.

--- src/unix/lwt_unix_stubs.c
+++ src/unix/lwt_unix_stubs.c
@@ -560,9 +560,11 @@ void lwt_unix_send_notification(int id)
 #if defined(LWT_ON_WINDOWS)
     if (ret == SOCKET_ERROR) {
       error = WSAGetLastError();
-      lwt_unix_mutex_unlock(&notification_mutex);
-      win32_maperr(error);
-      uerror("send_notification", Nothing);
+      if (error != WSANOTINITIALISED) {
+	lwt_unix_mutex_unlock(&notification_mutex);
+	win32_maperr(error);
+	uerror("send_notification", Nothing);
+      } /* else we're probably shutting down, so ignore the error */
     }
 #else
     if (ret < 0) {
