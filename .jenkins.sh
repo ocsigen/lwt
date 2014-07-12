04e26f0a57e83b90d225c45ec9c6dc1e2d0c6182
Grégoire Henry <gregoire.henry@ocamlpro.com>
2014-07-12 22:11:44 +0200

    Update .jenkins.sh: add 'conf-libev'

--- .jenkins.sh
+++ .jenkins.sh
@@ -1,5 +1,5 @@
 opam pin add --no-action lwt .
-opam install ssl lablgtk text react
+opam install ssl lablgtk text react conf-libev
 opam install --deps-only lwt
 opam install --verbose lwt
 opam remove lwt
