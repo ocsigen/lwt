ocsigen/lwt

commit 3f3112a1415611cf0a82f6ca3518e85ca5ab57f8

Add a cli example.

ppx/ppx_lwt.mli
@@ -184,7 +184,12 @@ else
 
    By default, the debug mode is enabled. This means that the [backtrace] versions of the [bind], [finalize] and [catch] functions are used, enabling proper backtraces for the Lwt exceptions.
 
-   The debug mode can be disabled with the option [-no-debug].
+   The debug mode can be disabled with the option [-no-debug]:
+
+   {[
+$ ocamlfind ocamlc -package lwt.ppx \
+    -ppxopt lwt.ppx,-no-debug -linkpkg -o foo foo.ml
+   ]}
 
    {2 Sequence}
 

commit ffdb93fa29101134cd19b34ebd800e5c389076f5

Docs: fix broken Ppx_lwt page
Problem introduced in 3f3112a1415611cf0a82f6ca3518e85ca5ab57f8.

The blank line after "{v" is necessary to cause wikidoc to produce the
needed Wiki Creole markup. The same is true of the single space
preceding "v}".

Resolves #263.

[skip ci]

ppx/ppx_lwt.mli
@@ -186,10 +186,11 @@ else
 
    The debug mode can be disabled with the option [-no-debug]:
 
-   {[
+   {v
+
 $ ocamlfind ocamlc -package lwt.ppx \
     -ppxopt lwt.ppx,-no-debug -linkpkg -o foo foo.ml
-   ]}
+ v}
 
    {2 Sequence}
 