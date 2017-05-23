c3077bcd388a54b4b93d7ab9870e27e22e9553e3
Joseph Thomas <joseph.thomas@viasat.com>
2017-05-22 21:02:43 -0400

    Updated headers in src/logger and src/glib.

--- src/glib/lwt_glib.ml
+++ src/glib/lwt_glib.ml
@@ -1,6 +1,5 @@
-(* Lightweight thread library for OCaml
+(* OCaml promise library
  * http://www.ocsigen.org/lwt
- * Module glib
  * Copyright (C) 2009-2011 Jérémie Dimino
  *
  * This program is free software; you can redistribute it and/or modify
--- src/logger/lwt_log_rules.mli
+++ src/logger/lwt_log_rules.mli
@@ -1,6 +1,5 @@
-(* Lightweight thread library for OCaml
+(* OCaml promise library
  * http://www.ocsigen.org/lwt
- * Interface Lwt_log_rules
  * Copyright (C) 2010 Jérémie Dimino <jeremie@dimino.org>
  *
  * This program is free software; you can redistribute it and/or modify
