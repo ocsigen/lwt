353f5019829ec6feae10a1adb79168b357e16d08
andrewray <evilkidder@gmail.com>
2017-05-27 15:32:56 +0100

    for appveryor, run the tests through opam instead.

--- src/util/appveyor-build.sh
+++ src/util/appveyor-build.sh
@@ -1,15 +1,12 @@
 set -e
 set -x
 
+# install packages and run tests
 if [ "$SYSTEM" = cygwin ]
 then
-    opam install -y --keep-build-dir --verbose lwt lwt_react lwt_ssl
-    cd `opam config var lib`/../build/lwt.*
-    jbuilder runtest --only-packages lwt,lwt_react,lwt_ssl
+    opam install -y -t --verbose lwt lwt_react lwt_ssl
 else
-    opam install -y --keep-build-dir --verbose lwt lwt_react
-    cd `opam config var lib`/../build/lwt.*
-    jbuilder runtest --only-packages lwt,lwt_react
+    opam install -y -t --verbose lwt lwt_react
 fi
 
 ! opam list -i batteries
