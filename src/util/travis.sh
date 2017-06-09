a34b469623f7a4901504aa2eba92e3fed9c6f260
Anton Bachin <antonbachin@yahoo.com>
2017-06-09 00:21:44 -0500

    Travis: move 4.04 testing to 4.04.1
    [skip appveyor]

--- src/util/travis.sh
+++ src/util/travis.sh
@@ -85,7 +85,7 @@ case $COMPILER in
     4.01) OCAML_VERSION=4.01.0;;
     4.02) OCAML_VERSION=4.02.3;;
     4.03) OCAML_VERSION=4.03.0;;
-    4.04) OCAML_VERSION=4.04.0;;
+    4.04) OCAML_VERSION=4.04.1;;
     4.05) OCAML_VERSION=4.05.0+beta2;;
     system) OCAML_VERSION=`ocamlc -version`;;
        *) echo Unsupported compiler $COMPILER; exit 1;;
