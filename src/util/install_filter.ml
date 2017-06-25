f99c84992f4e52b9183ef86969284d7b5c03e350
Andrew Ray <evilkidder@gmail.com>
2017-06-25 18:51:24 +0100

    Add a script to post-process lwt.install (#420)
    This flattens the installation directory for the 3.1.0 release and
    hopefully helps maintains compatibility with various packages that
    dont link with lwt.XXX sublibraries correctly.
    
    We use a post-process step rather than a custom hand-written install
    file so that jbuilder can work out for us exactly which packages
    were built.
    
    The opam build step calls the post-processing script after running
    jbuilder.
    
    The META file is lightly modified to remove the directory
    specifications.  They have been commented out for now so they can
    be easily added back.
    
    Finally, the install and uninstall steps in the Makefile were modified
    to use opam-installer.  Invoking "jbuilder install" after the
    post-processing step didn't seem to work.

---
+++ src/util/install_filter.ml
@@ -0,0 +1,59 @@
+(* By default jbuilder will create sub-directories for each sub-project
+   in the main lwt library.  This is probably preferred overall, however,
+   we would like to avoid this change for (at least) 1 release cycle so
+   it can be announced first.
+
+   This script will be run after jbuilder and modify the generated
+   lwt.install file to remove the sub-directories.
+
+   This works in concert with removing (actually commenting out for now)
+   the "directory = ..." clauses in META.lwt.template.
+
+   Note that we could create a .install file by hand, however,
+   we would then have to deal with configuration options (ie does
+   camlp4,unix,threads etc exist).  This scheme lets jbuilder work
+   all that out for us.
+*)
+
+let filter_sub_path path s =
+  try
+    (* look for the string *)
+    let start = String.index s '{' in
+    let end_ = String.index_from s start '/' in
+    if String.sub s (start + 2) (end_ - start - 2) = path then
+      (* remove the path bit *)
+      let ofs = start + 3 + String.length path in
+      String.sub s 0 (start + 2) ^ String.sub s ofs (String.length s - ofs)
+    else
+      s
+  with _ -> s
+
+let sub_paths = [
+  "unix";
+  "preemptive";
+  "log";
+  "options";
+  "syntax";
+  "simple-top";
+  "ppx";
+]
+
+let filter_sub_paths s = List.fold_right filter_sub_path sub_paths s
+
+let main () =
+  let read_line file = try Some(input_line file) with End_of_file -> None in
+  let rec read_lines file =
+    match read_line file with
+    | Some(x) -> x :: read_lines file
+    | None -> []
+  in
+  let file = open_in "lwt.install" in
+  let lines = List.map filter_sub_paths (read_lines file) in
+  let () = close_in file in
+  let file = open_out "lwt.install" in
+  let () = List.iter (Printf.fprintf file "%s\n") lines in
+  let () = close_out file in
+  flush stdout
+
+let () = main ()
+
