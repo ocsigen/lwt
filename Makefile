ocsigen/lwt

commit b2c2e163ed15323e64071495dfd3fb9a4f43e671

Tests: don't require package react to be installed
make test now runs tests only for package lwt. There is a new target,
make test-all, that runs all tests, including for lwt_react. The all
target has been correspondingly renamed to build-all; this builds
package lwt and additional packages lwt_react, lwt_glib, lwt_ssl.

Fixes #422.

Makefile
@@ -12,14 +12,19 @@ build: check-config
 		--only-packages lwt \
 		@install
 
-# build everything
-.PHONY: all
-all: check-config
+# build everything, including additional packages
+.PHONY: build-all
+build-all: check-config
 	jbuilder build @install
 
-# run all unit tests
+# run unit tests for package lwt
 .PHONY: test
 test: check-config
+	jbuilder runtest --only-packages lwt
+
+# run all unit tests
+.PHONY: test-all
+test-all: check-config
 	jbuilder runtest
 
 # configuration

commit 24dd66f1e4c7ccfe274d8a703e1e3655bfa9985c

CI: fix Jenkins wikidoc manual build
A previous commit,

  b2c2e163ed15323e64071495dfd3fb9a4f43e671

broke the doc-api-wiki (and -html) Makefile targets when it renamed the
all target to build-all, but did not properly update it everywhere.

Makefile
@@ -47,14 +47,14 @@ doc:
 
 # Build HTML documentation with ocamldoc
 .PHONY: doc-api-html
-doc-api-html: all
+doc-api-html: build-all
 	make -C doc api/html/index.html
 
 # Build wiki documentation with wikidoc
 # requires ocaml 4.03.0 and pinning the repo
 # https://github.com/ocsigen/wikidoc
 .PHONY: doc-api-wiki
-doc-api-wiki: all
+doc-api-wiki: build-all
 	make -C doc api/wiki/index.wiki
 
 # Use opam-installer, rather than jbuilder while we need to