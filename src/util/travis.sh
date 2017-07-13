7e4de96b9f5450ce442fb54f4684cd3f87848644
ZackC <zack_coker@yahoo.com>
2017-07-13 18:13:46 -0400

    update compiler switch version to 4.05.0 (#446)

--- src/util/travis.sh
+++ src/util/travis.sh
@@ -87,7 +87,7 @@ case $COMPILER in
     4.02) OCAML_VERSION=4.02.3;;
     4.03) OCAML_VERSION=4.03.0;;
     4.04) OCAML_VERSION=4.04.2;;
-    4.05) OCAML_VERSION=4.05.0+rc1;;
+    4.05) OCAML_VERSION=4.05.0;;
     4.06) OCAML_VERSION=4.06.0+trunk;;
     system) OCAML_VERSION=`ocamlc -version`;;
        *) echo Unsupported compiler $COMPILER; exit 1;;
