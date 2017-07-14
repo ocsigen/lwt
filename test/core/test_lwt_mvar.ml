658598f8550600bfedb67893ab12c42fc44f24a3
Joe Thomas <jsthomas@protonmail.com>
2017-07-13 19:29:40 -0600

    Revised some tests.

--- test/core/test_lwt_mvar.ml
+++ test/core/test_lwt_mvar.ml
@@ -26,34 +26,65 @@ let suite =
     test "basic take"
       (fun () ->
          let x = Lwt_mvar.create 0 in
-         let y = Lwt_mvar.take x >>= (fun u -> return (u + 1)) in
-         return (y = Lwt.return 1)
+         let y = Lwt_mvar.take x in
+         return (y = Lwt.return 0)
+      );
+
+    test "blocking put"
+      (fun () ->
+         let x = Lwt_mvar.create 0 in
+         let y = Lwt_mvar.put x 1 >>= (fun _ -> return 1) in
+         return (y != Lwt.return 1)
       );
 
     test "put-take"
       (fun () ->
          let x = Lwt_mvar.create_empty () in
-         let y = Lwt_mvar.take x >>= (fun u -> return (u + 1)) in
-         let z = Lwt_mvar.put x 0 >>= (fun _ -> y) in
-         return (z = Lwt.return 1)
+         let z = Lwt_mvar.put x 0 >>= (fun _ -> Lwt_mvar.take x) in
+         return (z = Lwt.return 0)
       );
 
     test "take-put"
       (fun () ->
          let x = Lwt_mvar.create 0 in
-         let y = Lwt_mvar.take x >>= (fun u -> return (u + 1)) in
-         let z = Lwt_mvar.put x 2 >>= (fun _ -> y) in
-         return (z = Lwt.return 1)
+         let _ = Lwt_mvar.take x in
+         let z = Lwt_mvar.put x 1 >>= (fun _ -> return 2) in
+         return (z = Lwt.return 2)
       );
 
-     test "take-put-take"
+    test "enqueued writer"
       (fun () ->
-         let x = Lwt_mvar.create 0 in
+         let x = Lwt_mvar.create 1 in
+         let _ = Lwt_mvar.put x 2 in
          let y =
-           Lwt_mvar.take x >>= (fun _ ->
-           Lwt_mvar.take x >>= (fun u ->
-             return (u + 1))) in
-         let z = Lwt_mvar.put x 2 >>= (fun _ -> y) in
-         return (z = Lwt.return 3)
+           Lwt_mvar.take x >>= (fun v1 ->
+           Lwt_mvar.take x >>= (fun v2 ->
+             Lwt.return (v1 + v2))) in
+         return (y = Lwt.return 3)
+      );
+
+    test "writer cancellation"
+      (fun () ->
+         let x = Lwt_mvar.create_empty () in
+         let y = Lwt_mvar.create 1 in
+         let t, _ = Lwt.task () in
+         let r1 = t >>= (fun _ -> Lwt_mvar.put y 2) in
+         let after_cancel () =
+           let _ = Lwt_mvar.take y >>= (fun z -> Lwt_mvar.put x z); in
+           () in
+         let _ = Lwt.on_cancel r1 after_cancel in
+         Lwt.cancel r1;
+         return (Lwt_mvar.take x = Lwt.return 1)
+      );
+
+    test "reader cancellation"
+      (fun () ->
+         let x = Lwt_mvar.create_empty () in
+         let t, _ = Lwt.task () in
+         let r1 = t >>= (fun _ -> Lwt_mvar.take x) in
+         let r2 = Lwt_mvar.take x in
+         let _ = Lwt.on_cancel r1 (fun _ -> let _ = Lwt_mvar.put x 1; in ()) in
+         Lwt.cancel r1;
+         return (r2 = Lwt.return 1)
       );
   ]
