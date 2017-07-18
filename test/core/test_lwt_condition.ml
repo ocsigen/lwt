8fa3ba32f6eab80adc63c108de8c79d06c608139
Joe Thomas <jsthomas@protonmail.com>
2017-07-17 19:25:34 -0600

    Increased coverage to 100%

--- test/core/test_lwt_condition.ml
+++ test/core/test_lwt_condition.ml
@@ -19,7 +19,8 @@
  *)
 
 open Test
-open Lwt
+
+exception Dummy_error
 
 let suite =
   suite "lwt_condition" [
@@ -28,17 +29,27 @@ let suite =
          let c = Lwt_condition.create () in
          let w = Lwt_condition.wait c in
          let () = Lwt_condition.signal c 1 in
-         return (w = Lwt.return 1)
+         Lwt.return (Lwt.state w = Lwt.Return 1)
       );
 
-    test "mutex wait"
+    test "mutex unlocked during wait"
+      (fun () ->
+         let c = Lwt_condition.create () in
+         let m = Lwt_mutex.create () in
+         let _ = Lwt_mutex.lock m in
+         let w = Lwt_condition.wait ~mutex:m c in
+         Lwt.return (Lwt.state w = Lwt.Sleep
+                     && not (Lwt_mutex.is_locked m))
+      );
+
+    test "mutex relocked after wait"
       (fun () ->
          let c = Lwt_condition.create () in
          let m = Lwt_mutex.create () in
          let _ = Lwt_mutex.lock m in
          let w = Lwt_condition.wait ~mutex:m c in
          let () = Lwt_condition.signal c 1 in
-         return ((w = Lwt.return 1) && (Lwt_mutex.is_locked m))
+         Lwt.return (Lwt.state w = Lwt.Return 1 && Lwt_mutex.is_locked m)
       );
 
     test "signal is not sticky"
@@ -46,8 +57,7 @@ let suite =
          let c = Lwt_condition.create () in
          let () = Lwt_condition.signal c 1 in
          let w = Lwt_condition.wait c in
-         return (Lwt.state w = Lwt.Sleep));
-
+         Lwt.return (Lwt.state w = Lwt.Sleep));
 
     test "broadcast"
       (fun () ->
@@ -55,7 +65,17 @@ let suite =
          let w1 = Lwt_condition.wait c in
          let w2 = Lwt_condition.wait c in
          let () = Lwt_condition.broadcast c 1 in
-         return ((w1 = Lwt.return 1) && (w2 = Lwt.return 1))
+         Lwt.return (Lwt.state w1 = Lwt.Return 1
+                     && Lwt.state w2 = Lwt.Return 1)
       );
 
+    test "broadcast exception"
+      (fun () ->
+         let c = Lwt_condition.create () in
+         let w1 = Lwt_condition.wait c in
+         let w2 = Lwt_condition.wait c in
+         let () = Lwt_condition.broadcast_exn c Dummy_error in
+         Lwt.return (Lwt.state w1 = Lwt.Fail Dummy_error
+                     && Lwt.state w2 = Lwt.Fail Dummy_error)
+      );
   ]
