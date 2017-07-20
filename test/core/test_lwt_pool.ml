3e2654e753bc7970adb0eb70383eb46b04cc5485
Joe Thomas <jsthomas@protonmail.com>
2017-07-19 20:54:20 -0600

    Increased coverage to 88.89%, needs refactoring

--- test/core/test_lwt_pool.ml
+++ test/core/test_lwt_pool.ml
@@ -59,11 +59,65 @@ let suite =
     test "validation"
       (fun () ->
          let gen = (fun () -> let l = ref [] in Lwt.return l) in
-         let v = (fun x -> Lwt.return (List.length !x = 0)) in
+         let v l = Lwt.return (List.length !l = 0) in
          let p = Lwt_pool.create 1 ~validate:v gen in
          let _ = Lwt_pool.use p (fun l -> l := 1 :: !l; Lwt.return (List.length !l)) in
          let u2 = Lwt_pool.use p (fun l -> Lwt.return (List.length !l)) in
          Lwt.return (Lwt.state u2 = Lwt.Return 0)
       );
 
+     test "failed validation"
+      (fun () ->
+         let gen = (fun () -> let l = ref [] in Lwt.return l) in
+         let v l =
+           if List.length! l == 0
+           then Lwt.return true
+           else Lwt.fail Dummy_error
+         in
+         let p = Lwt_pool.create 1 ~validate:v gen in
+         let u1 = Lwt_pool.use p (fun l -> l := 1 :: !l; Lwt.return (List.length !l)) in
+         let _ = Lwt_pool.use p (fun l -> Lwt.return (List.length !l)) in
+         Lwt.return (Lwt.state u1 = Lwt.Return 1) (*  && Lwt.state u2 = Lwt.Return 0) *)
+      );
+
+    test "multiple creation"
+      (fun () ->
+         let gen = (fun () -> let l = ref [] in Lwt.return l) in
+         let p = Lwt_pool.create 2 gen in
+         let _ = Lwt_pool.use p (fun l -> l := 1 :: !l; Lwt.pause ()) in
+         let u2 = Lwt_pool.use p (fun l -> Lwt.return (List.length !l)) in
+         Lwt.return (Lwt.state u2 = Lwt.Return 0)
+      );
+
+    test "limited creation of pool elements"
+      (fun () ->
+         let gen = (fun () -> let l = [] in Lwt.return l) in
+         let p = Lwt_pool.create 1 gen in
+         let _ = Lwt_pool.use p (fun _ -> Lwt.pause ()) in
+         let u2 = Lwt_pool.use p (fun l -> Lwt.return (List.length l)) in
+         Lwt.return (Lwt.state u2 = Lwt.Sleep)
+      );
+
+    test "check, elt ok"
+      (fun () ->
+         let gen = (fun () -> let l = ref [] in Lwt.return l) in
+         let c = (fun x f -> f (List.length !x > 0)) in
+         let p = Lwt_pool.create 1 ~check: c gen in
+         let task = (fun l -> l := 1 :: !l; Lwt.return (List.length !l)) in
+         let _ = Lwt_pool.use p (fun l -> l := 1 :: !l; Lwt.fail Dummy_error) in
+         let u2 = Lwt_pool.use p task in
+         Lwt.return (Lwt.state u2 = Lwt.Return 2)
+      );
+
+    test "checked, elt bad"
+      (fun () ->
+         let gen = (fun () -> let l = ref [] in Lwt.return l) in
+         let c = (fun x f -> f (List.length !x = 0)) in
+         let p = Lwt_pool.create 1 ~check: c gen in
+         let task = (fun l -> l := 1 :: !l; Lwt.return (List.length !l)) in
+         let _ = Lwt_pool.use p (fun l -> l := 1 :: !l; Lwt.fail Dummy_error) in
+         let u2 = Lwt_pool.use p task in
+         let u3 = Lwt_pool.use p task in (* this can go *)
+         Lwt.return (Lwt.state u2 = Lwt.Return 1 && Lwt.state u3 = Lwt.Return 2)
+      );
   ]
