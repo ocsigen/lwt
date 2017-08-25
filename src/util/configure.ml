911f529fae7254bc9405f0481f7c0249afff7f3d
Anton Bachin <antonbachin@yahoo.com>
2017-08-25 16:41:21 -0500

    Lightly reformat configure.ml
    [skip ci]

--- src/util/configure.ml
+++ src/util/configure.ml
@@ -12,12 +12,21 @@ let arg_bool r =
               | "true" -> r := Some true
               | "false" -> r := Some false
               | _ -> assert false)
+
+let usage =
+  "enable lwt.unix and camlp4 features\noptions are:"
+
 let args = [
-  "-use-libev", arg_bool use_libev, " whether to check for libev";
-  "-use-pthread", arg_bool use_pthread, " whether to use pthread";
-  "-android-target", arg_bool android_target, " compile for Android";
-  "-libev-default", arg_bool libev_default, " whether to use the libev backend by default";
-  "-use-camlp4", arg_bool use_camlp4, " when true enable camlp4 syntax extension";
+  "-use-libev", arg_bool use_libev,
+    " whether to check for libev";
+  "-use-pthread", arg_bool use_pthread,
+    " whether to use pthread";
+  "-android-target", arg_bool android_target,
+    " compile for Android";
+  "-libev-default", arg_bool libev_default,
+    " whether to use the libev backend by default";
+  "-use-camlp4", arg_bool use_camlp4,
+    " when true enable camlp4 syntax extension";
 ]
 
 let oasis_files = [
@@ -55,28 +64,32 @@ let oasis_sanity_check () =
   else ()
 
 let main () =
-  Arg.parse args ignore "enable lwt.unix and camlp4 features\noptions are:";
+  Arg.parse args ignore usage;
+
   let () = oasis_sanity_check () in
+
   let f = open_out "src/unix/lwt_config" in
   let print name var =
     match var with
     | None -> ()
     | Some var -> Printf.fprintf f "%s: %b\n" name var
   in
-  print"use_libev" !use_libev;
-  print"use_pthread" !use_pthread;
-  print"android_target" !android_target;
-  print"libev_default" !libev_default;
+  print "use_libev" !use_libev;
+  print "use_pthread" !use_pthread;
+  print "android_target" !android_target;
+  print "libev_default" !libev_default;
   close_out f;
-  (* '-use-camlp4 false' (or none) will write a jbuild-ignore file directing jbuilder to ignore
-   *                     the camlp4 directory.
-   * '-use-camlp4 true' will write an empty file which does nothing
-   *
-   * This is a workaround required to overcome some weird camlp4 packaging behaviour
-   * where ocamlfind sometimes installs a dummy META file for it even though it's
-   * not actually installed. *)
+
+  (* '-use-camlp4 false' (or none) will write a jbuild-ignore file directing
+                         jbuilder to ignore the camlp4 directory.
+     '-use-camlp4 true' will write an empty file which does nothing.
+
+     This is a workaround required to overcome some weird camlp4 packaging
+     behaviour where ocamlfind sometimes installs a dummy META file for it even
+     though it's not actually installed. *)
   let f = open_out "src/jbuild-ignore" in
-  (if !use_camlp4 = Some(true) then () else Printf.fprintf f "camlp4");
+  (if !use_camlp4 = Some true then () else Printf.fprintf f "camlp4");
   close_out f
 
-let () = main ()
+let () =
+  main ()
