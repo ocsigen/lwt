02468411867a4c10930ec26f451623d8292d1d97
Joe Thomas <jsthomas@protonmail.com>
2017-08-26 07:41:47 -0600

    Fixed typo in comment.

--- test/unix/test_lwt_io.ml
+++ test/unix/test_lwt_io.ml
@@ -1,7 +1,7 @@
 (* OCaml promise library
  * http://www.ocsigen.org/lwt
  * Copyright (C) 2009 Jérémie Dimino
-n *
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, with linking exceptions;
