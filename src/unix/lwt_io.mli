736b45594bc7ce189a09345701d0b6c70516f88a
Anton Bachin <antonbachin@yahoo.com>
2017-08-29 09:52:57 -0500

    Tweaks after preceding commit
    - Don't cause side effects in Lwt_io.open_temp_file before it is fully
      applied.
    - Adjust whitespace.
    - Minor tweaks, such as using the unit pattern instead of wildcard, etc.

--- src/unix/lwt_io.mli
+++ src/unix/lwt_io.mli
@@ -405,20 +405,22 @@ val with_file :
       channel is closed when [f ch] terminates (even if it fails). *)
 
 val open_temp_file :
-  ?buffer : Lwt_bytes.t ->
-  ?flags : Unix.open_flag list ->
-  ?perm : Unix.file_perm ->
-  ?temp_dir : string ->
-  ?prefix : string ->
-  unit -> (string * output_channel) Lwt.t
+  ?buffer:Lwt_bytes.t ->
+  ?flags:Unix.open_flag list ->
+  ?perm:Unix.file_perm ->
+  ?temp_dir:string ->
+  ?prefix:string ->
+  unit ->
+    (string * output_channel) Lwt.t
 
 val with_temp_file :
   ?buffer:Lwt_bytes.t ->
-  ?flags : Unix.open_flag list ->
-  ?perm : Unix.file_perm ->
-  ?temp_dir : string ->
-  ?prefix : string ->
-  (string * output_channel -> 'b Lwt.t) -> 'b Lwt.t
+  ?flags:Unix.open_flag list ->
+  ?perm:Unix.file_perm ->
+  ?temp_dir:string ->
+  ?prefix:string ->
+  (string * output_channel -> 'b Lwt.t) ->
+    'b Lwt.t
 
 val open_connection :
   ?fd : Lwt_unix.file_descr ->
