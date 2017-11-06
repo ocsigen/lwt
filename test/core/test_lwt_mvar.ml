a8f8d191fa3cb6bed0a7e2623a3bc1bcab0818aa
Anton Bachin <antonbachin@yahoo.com>
2017-11-06 08:19:32 -0600

    Adjust whitespace in Test_lwt_mvar
    [skip ci]

--- test/core/test_lwt_mvar.ml
+++ test/core/test_lwt_mvar.ml
@@ -18,90 +18,80 @@
  * 02111-1307, USA.
  *)
 
+
+
 open Test
 
-let suite =
-  suite "lwt_mvar" [
-    test "basic take"
-      (fun () ->
-         let x = Lwt_mvar.create 0 in
-         let y = Lwt_mvar.take x in
-         Lwt.return (Lwt.state y = Lwt.Return 0)
-      );
+let suite = suite "lwt_mvar" [
+  test "basic take" begin fun () ->
+    let x = Lwt_mvar.create 0 in
+    let y = Lwt_mvar.take x in
+    Lwt.return (Lwt.state y = Lwt.Return 0)
+  end;
 
-    test "take_available (full)"
-      (fun () ->
-         let x = Lwt_mvar.create 0 in
-         let y = Lwt_mvar.take_available x in
-         Lwt.return (y = Some 0)
-      );
+  test "take_available (full)" begin fun () ->
+    let x = Lwt_mvar.create 0 in
+    let y = Lwt_mvar.take_available x in
+    Lwt.return (y = Some 0)
+  end;
 
-    test "take_available (empty)"
-      (fun () ->
-         let x = Lwt_mvar.create_empty () in
-         let y = Lwt_mvar.take_available x in
-         Lwt.return (y = None)
-      );
+  test "take_available (empty)" begin fun () ->
+    let x = Lwt_mvar.create_empty () in
+    let y = Lwt_mvar.take_available x in
+    Lwt.return (y = None)
+  end;
 
-    test "take_available (twice)"
-      (fun () ->
-         let x = Lwt_mvar.create 0 in
-         let (_ : int option) = Lwt_mvar.take_available x in
-         let y = Lwt_mvar.take_available x in
-         Lwt.return (y = None)
-      );
+  test "take_available (twice)" begin fun () ->
+    let x = Lwt_mvar.create 0 in
+    let (_ : int option) = Lwt_mvar.take_available x in
+    let y = Lwt_mvar.take_available x in
+    Lwt.return (y = None)
+  end;
 
-    test "is_empty (full)"
-      (fun () ->
-         let x = Lwt_mvar.create 0 in
-         let y = Lwt_mvar.is_empty x in
-         Lwt.return (not y)
-      );
+  test "is_empty (full)" begin fun () ->
+    let x = Lwt_mvar.create 0 in
+    let y = Lwt_mvar.is_empty x in
+    Lwt.return (not y)
+  end;
 
-    test "is_empty (empty)"
-      (fun () ->
-         let x = Lwt_mvar.create_empty () in
-         let y = Lwt_mvar.is_empty x in
-         Lwt.return y
-      );
+  test "is_empty (empty)" begin fun () ->
+    let x = Lwt_mvar.create_empty () in
+    let y = Lwt_mvar.is_empty x in
+    Lwt.return y
+  end;
 
-    test "blocking put"
-      (fun () ->
-         let x = Lwt_mvar.create 0 in
-         let y = Lwt_mvar.put x 1 in
-         Lwt.return (Lwt.state y = Lwt.Sleep)
-      );
+  test "blocking put" begin fun () ->
+    let x = Lwt_mvar.create 0 in
+    let y = Lwt_mvar.put x 1 in
+    Lwt.return (Lwt.state y = Lwt.Sleep)
+  end;
 
-    test "put-take"
-      (fun () ->
-         let x = Lwt_mvar.create_empty () in
-         let _ = Lwt_mvar.put x 0 in
-         let y = Lwt_mvar.take x in
-         Lwt.return (Lwt.state y = Lwt.Return 0)
-      );
+  test "put-take" begin fun () ->
+    let x = Lwt_mvar.create_empty () in
+    let _ = Lwt_mvar.put x 0 in
+    let y = Lwt_mvar.take x in
+    Lwt.return (Lwt.state y = Lwt.Return 0)
+  end;
 
-    test "take-put"
-      (fun () ->
-         let x = Lwt_mvar.create 0 in
-         let _ = Lwt_mvar.take x in
-         let y = Lwt_mvar.put x 1 in
-         Lwt.return (Lwt.state y = Lwt.Return ())
-      );
+  test "take-put" begin fun () ->
+    let x = Lwt_mvar.create 0 in
+    let _ = Lwt_mvar.take x in
+    let y = Lwt_mvar.put x 1 in
+    Lwt.return (Lwt.state y = Lwt.Return ())
+  end;
 
-    test "enqueued writer"
-      (fun () ->
-         let x = Lwt_mvar.create 1 in
-         let y = Lwt_mvar.put x 2 in
-         let z = Lwt_mvar.take x in
-         Lwt.return (Lwt.state y = Lwt.Return ()
-                     && Lwt.state z = Lwt.Return 1)
-      );
+  test "enqueued writer" begin fun () ->
+    let x = Lwt_mvar.create 1 in
+    let y = Lwt_mvar.put x 2 in
+    let z = Lwt_mvar.take x in
+    Lwt.return (Lwt.state y = Lwt.Return () && Lwt.state z = Lwt.Return 1)
+  end;
 
-    test "writer cancellation"
-      (fun () ->
-         let y = Lwt_mvar.create 1 in
-         let r1 = Lwt_mvar.put y 2 in
-         Lwt.cancel r1;
-         Lwt.return ((Lwt.state (Lwt_mvar.take y) = Lwt.Return 1)
-                     && (Lwt.state (Lwt_mvar.take y) = Lwt.Sleep)));
+  test "writer cancellation" begin fun () ->
+    let y = Lwt_mvar.create 1 in
+    let r1 = Lwt_mvar.put y 2 in
+    Lwt.cancel r1;
+    Lwt.return ((Lwt.state (Lwt_mvar.take y) = Lwt.Return 1)
+                && (Lwt.state (Lwt_mvar.take y) = Lwt.Sleep))
+  end;
   ]
