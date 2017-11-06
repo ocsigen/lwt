b9b59cca1ca1a9d4354517b9da16c22f7ac36ec7
Anton Bachin <antonbachin@yahoo.com>
2017-11-06 08:32:19 -0600

    Test_lwt_mvar: use asynchronous state query

--- test/core/test_lwt_mvar.ml
+++ test/core/test_lwt_mvar.ml
@@ -20,13 +20,21 @@
 
 
 
+open Lwt.Infix
 open Test
+
+
+
+let state_is =
+  Lwt.debug_state_is
+
+
 
 let suite = suite "lwt_mvar" [
   test "basic take" begin fun () ->
     let x = Lwt_mvar.create 0 in
     let y = Lwt_mvar.take x in
-    Lwt.return (Lwt.state y = Lwt.Return 0)
+    state_is (Lwt.Return 0) y
   end;
 
   test "take_available (full)" begin fun () ->
@@ -70,21 +78,23 @@ let suite = suite "lwt_mvar" [
     let x = Lwt_mvar.create_empty () in
     let _ = Lwt_mvar.put x 0 in
     let y = Lwt_mvar.take x in
-    Lwt.return (Lwt.state y = Lwt.Return 0)
+    state_is (Lwt.Return 0) y
   end;
 
   test "take-put" begin fun () ->
     let x = Lwt_mvar.create 0 in
     let _ = Lwt_mvar.take x in
     let y = Lwt_mvar.put x 1 in
-    Lwt.return (Lwt.state y = Lwt.Return ())
+    state_is (Lwt.Return ()) y
   end;
 
   test "enqueued writer" begin fun () ->
     let x = Lwt_mvar.create 1 in
     let y = Lwt_mvar.put x 2 in
     let z = Lwt_mvar.take x in
-    Lwt.return (Lwt.state y = Lwt.Return () && Lwt.state z = Lwt.Return 1)
+    state_is (Lwt.Return ()) y >>= fun y_correct ->
+    state_is (Lwt.Return 1) z >>= fun z_correct ->
+    Lwt.return (y_correct && z_correct)
   end;
 
   test "writer cancellation" begin fun () ->
