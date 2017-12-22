e1ba1a8ce5925b3d71a4f60e05b5c895736aaac0
cedlemo <cedlemo@gmx.com>
2017-12-22 12:13:05 +0100

    Split stat job related C bindings in their own C files

--- src/unix/lwt_unix_windows.h
+++ src/unix/lwt_unix_windows.h
@@ -27,80 +27,6 @@ CAMLprim value lwt_unix_is_socket(value fd)
 }
 
 /* +-----------------------------------------------------------------+
-   | JOB: bytes_read                                                 |
-   +-----------------------------------------------------------------+ */
-
-
-
-/* +-----------------------------------------------------------------+
-   | JOB: write                                                      |
-   +-----------------------------------------------------------------+ */
-
-
-
-/* +-----------------------------------------------------------------+
-   | JOB: bytes_write                                                |
-   +-----------------------------------------------------------------+ */
-
-struct job_bytes_write {
-    struct lwt_unix_job job;
-    union {
-        HANDLE handle;
-        SOCKET socket;
-    } fd;
-    int kind;
-    char *buffer;
-    DWORD length;
-    DWORD result;
-    DWORD error_code;
-};
-
-static void worker_bytes_write(struct job_bytes_write *job)
-{
-    if (job->kind == KIND_SOCKET) {
-        int ret;
-        ret = send(job->fd.socket, job->buffer, job->length, 0);
-        if (ret == SOCKET_ERROR) job->error_code = WSAGetLastError();
-        job->result = ret;
-    } else {
-        if (!WriteFile(job->fd.handle, job->buffer, job->length, &(job->result),
-                       NULL))
-            job->error_code = GetLastError();
-    }
-}
-
-CAMLprim value result_bytes_write(struct job_bytes_write *job)
-{
-    value result;
-    DWORD error = job->error_code;
-    if (error) {
-        lwt_unix_free_job(&job->job);
-        win32_maperr(error);
-        uerror("bytes_write", Nothing);
-    }
-    result = Val_long(job->result);
-    lwt_unix_free_job(&job->job);
-    return result;
-}
-
-CAMLprim value lwt_unix_bytes_write_job(value val_fd, value val_buffer,
-                                        value val_offset, value val_length)
-{
-    struct filedescr *fd = (struct filedescr *)Data_custom_val(val_fd);
-    LWT_UNIX_INIT_JOB(job, bytes_write, 0);
-    job->job.worker = (lwt_unix_job_worker)worker_bytes_write;
-    job->kind = fd->kind;
-    if (fd->kind == KIND_HANDLE)
-        job->fd.handle = fd->fd.handle;
-    else
-        job->fd.socket = fd->fd.socket;
-    job->buffer = (char *)Caml_ba_data_val(val_buffer) + Long_val(val_offset);
-    job->length = Long_val(val_length);
-    job->error_code = 0;
-    return lwt_unix_alloc_job(&(job->job));
-}
-
-/* +-----------------------------------------------------------------+
    | JOB: fsync                                                      |
    +-----------------------------------------------------------------+ */
 
@@ -194,14 +120,8 @@ CAMLprim value lwt_unix_system_job(value cmdline)
    | Unavailable primitives                                          |
    +-----------------------------------------------------------------+ */
 
-LWT_NOT_AVAILABLE1(unix_stat_job)
-LWT_NOT_AVAILABLE1(unix_lstat_job)
-LWT_NOT_AVAILABLE1(unix_fstat_job)
 LWT_NOT_AVAILABLE3(unix_utimes_job)
 LWT_NOT_AVAILABLE1(unix_isatty_job)
-LWT_NOT_AVAILABLE1(unix_stat_64_job)
-LWT_NOT_AVAILABLE1(unix_lstat_64_job)
-LWT_NOT_AVAILABLE1(unix_fstat_64_job)
 LWT_NOT_AVAILABLE1(unix_opendir_job)
 LWT_NOT_AVAILABLE1(unix_readdir_job)
 LWT_NOT_AVAILABLE2(unix_readdir_n_job)
---
+++ src/unix/unix_c/unix_fstat_64_job.c
@@ -0,0 +1,41 @@
+/* OCaml promise library
+ * http://www.ocsigen.org/lwt
+ * Copyright (C) 2009-2010 Jérémie Dimino
+ *               2009 Mauricio Fernandez
+ *               2010 Pierre Chambart
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, with linking exceptions;
+ * either version 2.1 of the License, or (at your option) any later
+ * version. See COPYING file for details.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ */
+
+#include "lwt_config.h"
+
+#if !defined(LWT_ON_WINDOWS)
+
+#include <caml/mlvalues.h>
+#include <caml/unixsupport.h>
+
+#include "lwt_unix.h"
+#include "unix_stat_job_utils.h"
+
+CAMLprim value lwt_unix_fstat_64_job(value val_fd)
+{
+    LWT_UNIX_INIT_JOB(job, fstat, 0);
+    job->job.result = (lwt_unix_job_result)result_fstat_64;
+    job->fd = Int_val(val_fd);
+    return lwt_unix_alloc_job(&(job->job));
+}
+#endif
