2a993351c66332fa8d2d2f9d767c46d1ae45566f
Anton Bachin <antonbachin@yahoo.com>
2018-01-02 10:00:13 -0500

    Move Lwt_ssl to its own repository
    It can now be found at
    
      https://github.com/aantron/lwt_ssl

--- src/util/appveyor-build.sh
+++ src/util/appveyor-build.sh
@@ -2,11 +2,6 @@ set -e
 set -x
 
 # install packages and run tests
-if [ "$SYSTEM" = cygwin ]
-then
-    opam install -y -t --verbose lwt lwt_react lwt_ssl lwt_camlp4
-else
-    opam install -y -t --verbose lwt lwt_react lwt_camlp4
-fi
+opam install -y -t --verbose lwt lwt_react lwt_camlp4
 
 ! opam list -i batteries
