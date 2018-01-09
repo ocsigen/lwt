98fe3050eeb2aba76c5633259b727b3cec836674
cedlemo <cedlemo@gmx.com>
2018-01-09 22:08:42 +0100

    Lwt_sequence: create set of tests for create/add*/take* functions (#406)

--- test/core/test_lwt_sequence.ml
+++ test/core/test_lwt_sequence.ml
@@ -22,6 +22,86 @@
 
 open Test
 
+[@@@ocaml.warning "-3"]
+module Lwt_sequence = Lwt_sequence
+[@@@ocaml.warning "+3"]
+
 let suite = suite "lwt_sequence" [
 
+  test "create" begin fun () ->
+    let s = Lwt_sequence.create () in
+    let _ = assert (Lwt_sequence.is_empty s) in
+    let len = Lwt_sequence.length s in
+    Lwt.return (len = 0)
+  end;
+
+  test "add_l" begin fun () ->
+    let s = Lwt_sequence.create () in
+    let n = Lwt_sequence.add_l 1 s in
+    let _ = assert ((Lwt_sequence.get n) = 1) in
+    let len = Lwt_sequence.length s in
+    Lwt.return (len = 1)
+  end;
+
+  test "add_r" begin fun () ->
+    let s = Lwt_sequence.create () in
+    let n = Lwt_sequence.add_r 1 s in
+    let _ = assert ((Lwt_sequence.get n) = 1) in
+    let len = Lwt_sequence.length s in
+    Lwt.return (len = 1)
+  end;
+
+  test "take_l Empty" begin fun () ->
+    let s = Lwt_sequence.create () in
+    Lwt.catch
+      (fun () ->
+        let _ = Lwt_sequence.take_l s in
+        Lwt.return_false
+      )
+      (function
+        | Lwt_sequence.Empty -> Lwt.return_true
+        | _ -> Lwt.return_false
+      )
+  end;
+
+  test "take_l" begin fun () ->
+    let s = Lwt_sequence.create () in
+    let v = 1 in
+    let _ = Lwt_sequence.add_l v s in
+    Lwt.catch
+      (fun () ->
+        let v' = Lwt_sequence.take_l s in
+        Lwt.return (v' = v)
+      )
+      (function
+        | _ -> Lwt.return_false
+      )
+  end;
+
+  test "take_r Empty" begin fun () ->
+    let s = Lwt_sequence.create () in
+    Lwt.catch
+      (fun () ->
+        let _ = Lwt_sequence.take_r s in
+        Lwt.return_false
+      )
+      (function
+        | Lwt_sequence.Empty -> Lwt.return_true
+        | _ -> Lwt.return_false
+      )
+  end;
+
+  test "take_r" begin fun () ->
+    let s = Lwt_sequence.create () in
+    let v = 1 in
+    let _ = Lwt_sequence.add_r v s in
+    Lwt.catch
+      (fun () ->
+        let v' = Lwt_sequence.take_r s in
+        Lwt.return (v' = v)
+      )
+      (function
+        | _ -> Lwt.return_false
+      )
+  end;
 ]
