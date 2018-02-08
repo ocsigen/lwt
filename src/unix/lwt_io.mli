bba9ead8227aef5f1c3650c65298ce1a64301aa8
Jess Smith <smith.jessk@gmail.com>
2018-02-07 23:06:18 -0500

    Document Lwt_io.printf flushing behavior

--- src/unix/lwt_io.mli
+++ src/unix/lwt_io.mli
@@ -360,16 +360,30 @@ val write_value : output_channel -> ?flags : Marshal.extern_flags list -> 'a -> 
 
 val fprint : output_channel -> string -> unit Lwt.t
 val fprintl : output_channel -> string -> unit Lwt.t
+
 val fprintf : output_channel -> ('a, unit, string, unit Lwt.t) format4 -> 'a
+(* %! does nothing here. To flush the channel, use Lwt_io.(flush channel) *)
+
 val fprintlf : output_channel -> ('a, unit, string, unit Lwt.t) format4 -> 'a
+(* %! does nothing here. To flush the channel, use Lwt_io.(flush channel) *)
+
 val print : string -> unit Lwt.t
 val printl : string -> unit Lwt.t
+
 val printf : ('a, unit, string, unit Lwt.t) format4 -> 'a
+(* %! does nothing here. To flush the channel, use Lwt_io.(flush channel) *)
+
 val printlf : ('a, unit, string, unit Lwt.t) format4 -> 'a
+(* %! does nothing here. To flush the channel, use Lwt_io.(flush channel) *)
+
 val eprint : string -> unit Lwt.t
 val eprintl : string -> unit Lwt.t
+
 val eprintf : ('a, unit, string, unit Lwt.t) format4 -> 'a
+(* %! does nothing here. To flush the channel, use Lwt_io.(flush channel) *)
+
 val eprintlf : ('a, unit, string, unit Lwt.t) format4 -> 'a
+(* %! does nothing here. To flush the channel, use Lwt_io.(flush channel) *)
 
 (** {2 Utilities} *)
 
