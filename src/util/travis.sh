7c0d58060675c9aa592aabd68f88543b2c82a9d9
Anton Bachin <antonbachin@yahoo.com>
2018-04-17 18:41:00 -0500

    Travis: test on 4.07.0+beta2
    [skip appveyor]

--- src/util/travis.sh
+++ src/util/travis.sh
@@ -11,6 +11,7 @@ packages_apt () {
         4.04) PPA=avsm/ocaml42+opam12; DO_SWITCH=yes;;
         4.05) PPA=avsm/ocaml42+opam12; DO_SWITCH=yes;;
         4.06) PPA=avsm/ocaml42+opam12; DO_SWITCH=yes;;
+        4.07) PPA=avsm/ocaml42+opam12; DO_SWITCH=yes;;
            *) echo Unsupported compiler $COMPILER; exit 1;;
     esac
 
@@ -93,6 +94,7 @@ case $COMPILER in
     4.04) OCAML_VERSION=4.04.2;;
     4.05) OCAML_VERSION=4.05.0;;
     4.06) OCAML_VERSION=4.06.1;;
+    4.07) OCAML_VERSION=4.07.0+beta2;;
     system) OCAML_VERSION=`ocamlc -version`;;
        *) echo Unsupported compiler $COMPILER; exit 1;;
 esac
