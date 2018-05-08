e0861fa7a7cd63dcc34e2dda829d6853202aecb6
cedlemo <cedlemo@gmx.com>
2018-05-08 22:20:36 +0200

    Lwt_bytes : add tests for get/set unsafe_get/unsage_set

--- test/unix/test_lwt_bytes.ml
+++ test/unix/test_lwt_bytes.ml
@@ -10,4 +10,57 @@ let suite = suite "lwt_bytes" [
     let len' = Lwt_bytes.length buff in
     Lwt.return (len = len')
   end;
+
+  test "get/set" begin fun () ->
+    let buff = Lwt_bytes.create 4 in
+    let _ = Lwt_bytes.set buff 0 'a' in
+    let _ = Lwt_bytes.set buff 1 'b' in
+    let _ = Lwt_bytes.set buff 2 'c' in
+    let check = Lwt_bytes.get buff 0 = 'a' &&
+    Lwt_bytes.get buff 1 = 'b' &&
+    Lwt_bytes.get buff 2 = 'c'
+    in Lwt.return check
+  end;
+
+  test "get out of bounds" begin fun () ->
+    let buff = Lwt_bytes.create 3 in
+    try
+      let _ = Lwt_bytes.get buff 150 in
+      Lwt.return false
+    with
+    | Invalid_argument _ -> Lwt.return true
+    | _ -> Lwt.return false
+  end;
+
+  test "set out of bounds" begin fun () ->
+    let buff = Lwt_bytes.create 3 in
+    try
+    let _ = Lwt_bytes.set buff 150 in
+    Lwt.return true
+    with
+    | Invalid_argument _ -> Lwt.return true
+    | _ -> Lwt.return false
+  end;
+
+  test "unsafe_get/unsage_set" begin fun () ->
+    let buff = Lwt_bytes.create 4 in
+    let _ = Lwt_bytes.unsafe_set buff 0 'a' in
+    let _ = Lwt_bytes.unsafe_set buff 1 'b' in
+    let _ = Lwt_bytes.unsafe_set buff 2 'c' in
+    let check = Lwt_bytes.unsafe_get buff 0 = 'a' &&
+    Lwt_bytes.unsafe_get buff 1 = 'b' &&
+    Lwt_bytes.unsafe_get buff 2 = 'c'
+    in Lwt.return check
+  end;
+
+  test "unsafe_get out of bounds" begin fun () ->
+    let buff = Lwt_bytes.create 3 in
+    (*try *)
+      let _ = Lwt_bytes.unsafe_get buff 150 in
+      Lwt.return true
+    (* with
+
+    | _ -> Lwt.return false *)
+  end;
+
 ]
