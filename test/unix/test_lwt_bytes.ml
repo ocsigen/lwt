0da671ee48d8c1177832397cab9dd1fda6f901dc
Cédric Le Moigne <cedlemo@gmx.com>
2018-09-08 20:59:21 +0200

    Fix style issues + Neoformat

--- test/unix/test_lwt_bytes.ml
+++ test/unix/test_lwt_bytes.ml
@@ -4,245 +4,245 @@
 open Test
 
 let suite = suite "lwt_bytes" [
-  test "create" begin fun () ->
-    let len = 5 in
-    let buff = Lwt_bytes.create len in
-    let len' = Lwt_bytes.length buff in
-    Lwt.return (len = len')
-  end;
+    test "create" begin fun () ->
+      let len = 5 in
+      let buff = Lwt_bytes.create len in
+      let len' = Lwt_bytes.length buff in
+      Lwt.return (len = len')
+    end;
 
-  test "get/set" begin fun () ->
-    let buff = Lwt_bytes.create 4 in
-    let _ = Lwt_bytes.set buff 0 'a' in
-    let _ = Lwt_bytes.set buff 1 'b' in
-    let _ = Lwt_bytes.set buff 2 'c' in
-    let check = Lwt_bytes.get buff 0 = 'a' &&
-    Lwt_bytes.get buff 1 = 'b' &&
-    Lwt_bytes.get buff 2 = 'c'
-    in Lwt.return check
-  end;
+    test "get/set" begin fun () ->
+      let buff = Lwt_bytes.create 4 in
+      let () = Lwt_bytes.set buff 0 'a' in
+      let () = Lwt_bytes.set buff 1 'b' in
+      let () = Lwt_bytes.set buff 2 'c' in
+      let check = Lwt_bytes.get buff 0 = 'a' &&
+                  Lwt_bytes.get buff 1 = 'b' &&
+                  Lwt_bytes.get buff 2 = 'c'
+      in Lwt.return check
+    end;
 
-  test "get out of bounds" begin fun () ->
-    let buff = Lwt_bytes.create 3 in
-    try
-      let _ = Lwt_bytes.get buff 150 in
-      Lwt.return false
-    with
-    | Invalid_argument _ -> Lwt.return true
-    | _ -> Lwt.return false
-  end;
+    test "get out of bounds" begin fun () ->
+      let buff = Lwt_bytes.create 3 in
+      try
+        let _ = Lwt_bytes.get buff 150 in
+        Lwt.return false
+      with
+      | Invalid_argument _ -> Lwt.return true
+      | _ -> Lwt.return false
+    end;
 
-  test "set out of bounds" begin fun () ->
-    let buff = Lwt_bytes.create 3 in
-    try
-    let _ = Lwt_bytes.set buff 150 in
-    Lwt.return true
-    with
-    | Invalid_argument _ -> Lwt.return true
-    | _ -> Lwt.return false
-  end;
+    test "set out of bounds" begin fun () ->
+      let buff = Lwt_bytes.create 3 in
+      try
+        let () = Lwt_bytes.set buff 150 'a' in
+        Lwt.return true
+      with
+      | Invalid_argument _ -> Lwt.return true
+      | _ -> Lwt.return false
+    end;
 
-  test "unsafe_get/unsage_set" begin fun () ->
-    let buff = Lwt_bytes.create 4 in
-    let _ = Lwt_bytes.unsafe_set buff 0 'a' in
-    let _ = Lwt_bytes.unsafe_set buff 1 'b' in
-    let _ = Lwt_bytes.unsafe_set buff 2 'c' in
-    let check = Lwt_bytes.unsafe_get buff 0 = 'a' &&
-    Lwt_bytes.unsafe_get buff 1 = 'b' &&
-    Lwt_bytes.unsafe_get buff 2 = 'c'
-    in Lwt.return check
-  end;
+    test "unsafe_get/unsage_set" begin fun () ->
+      let buff = Lwt_bytes.create 4 in
+      let () = Lwt_bytes.unsafe_set buff 0 'a' in
+      let () = Lwt_bytes.unsafe_set buff 1 'b' in
+      let () = Lwt_bytes.unsafe_set buff 2 'c' in
+      let check = Lwt_bytes.unsafe_get buff 0 = 'a' &&
+                  Lwt_bytes.unsafe_get buff 1 = 'b' &&
+                  Lwt_bytes.unsafe_get buff 2 = 'c'
+      in Lwt.return check
+    end;
 
-  test "of bytes" begin fun () ->
-    let bytes = Bytes.of_string "abc" in
-    let buff = Lwt_bytes.of_bytes bytes in
-    let check = Lwt_bytes.get buff 0 = Bytes.get bytes 0 &&
-                Lwt_bytes.get buff 1 = Bytes.get bytes 1 &&
-                Lwt_bytes.get buff 2 = Bytes.get bytes 2
-    in Lwt.return check
-  end;
+    test "of bytes" begin fun () ->
+      let bytes = Bytes.of_string "abc" in
+      let buff = Lwt_bytes.of_bytes bytes in
+      let check = Lwt_bytes.get buff 0 = Bytes.get bytes 0 &&
+                  Lwt_bytes.get buff 1 = Bytes.get bytes 1 &&
+                  Lwt_bytes.get buff 2 = Bytes.get bytes 2
+      in Lwt.return check
+    end;
 
-  test "of string" begin fun () ->
-    let buff = Lwt_bytes.of_string "abc" in
-    let check = Lwt_bytes.get buff 0 = 'a' &&
-                Lwt_bytes.get buff 1 = 'b' &&
-                Lwt_bytes.get buff 2 = 'c'
-    in Lwt.return check
-  end;
+    test "of string" begin fun () ->
+      let buff = Lwt_bytes.of_string "abc" in
+      let check = Lwt_bytes.get buff 0 = 'a' &&
+                  Lwt_bytes.get buff 1 = 'b' &&
+                  Lwt_bytes.get buff 2 = 'c'
+      in Lwt.return check
+    end;
 
-  test "to bytes" begin fun () ->
-    let bytes = Bytes.of_string "abc" in
-    let buff = Lwt_bytes.of_bytes bytes in
-    let bytes' = Lwt_bytes.to_bytes buff in
-    let check = Bytes.equal bytes bytes' in
-    Lwt.return check
-  end;
+    test "to bytes" begin fun () ->
+      let bytes = Bytes.of_string "abc" in
+      let buff = Lwt_bytes.of_bytes bytes in
+      let bytes' = Lwt_bytes.to_bytes buff in
+      let check = Bytes.equal bytes bytes' in
+      Lwt.return check
+    end;
 
-  test "to string" begin fun () ->
-    let str = "abc" in
-    let buff = Lwt_bytes.of_string str in
-    let str' = Lwt_bytes.to_string buff in
-    let check = str = str' in
-    Lwt.return check
-  end;
+    test "to string" begin fun () ->
+      let str = "abc" in
+      let buff = Lwt_bytes.of_string str in
+      let str' = Lwt_bytes.to_string buff in
+      let check = str = str' in
+      Lwt.return check
+    end;
 
-  test "blit" begin fun () ->
-    let str1 = "abc" in
-    let buf1 = Lwt_bytes.of_string str1 in
-    let str2 = "abcdef" in
-    let buf2 = Lwt_bytes.of_string str2 in
-    let () = Lwt_bytes.blit buf1 0 buf2 3 3 in
-    let check = "abcabc" = Lwt_bytes.to_string buf2 in
-    Lwt.return check
-  end;
+    test "blit" begin fun () ->
+      let str1 = "abc" in
+      let buf1 = Lwt_bytes.of_string str1 in
+      let str2 = "abcdef" in
+      let buf2 = Lwt_bytes.of_string str2 in
+      let () = Lwt_bytes.blit buf1 0 buf2 3 3 in
+      let check = "abcabc" = Lwt_bytes.to_string buf2 in
+      Lwt.return check
+    end;
 
-  test "blit out of bound" begin fun () ->
-    let str1 = "abc" in
-    let buf1 = Lwt_bytes.of_string str1 in
-    let str2 = "abcdef" in
-    let buf2 = Lwt_bytes.of_string str2 in
-    try
-      let() = Lwt_bytes.blit buf1 150 buf2 150 150 in
-      Lwt.return_false
-    with
-    | Invalid_argument _ -> Lwt.return true
-    | _ -> Lwt.return false
-  end;
+    test "blit out of bound" begin fun () ->
+      let str1 = "abc" in
+      let buf1 = Lwt_bytes.of_string str1 in
+      let str2 = "abcdef" in
+      let buf2 = Lwt_bytes.of_string str2 in
+      try
+        let() = Lwt_bytes.blit buf1 150 buf2 150 150 in
+        Lwt.return_false
+      with
+      | Invalid_argument _ -> Lwt.return true
+      | _ -> Lwt.return false
+    end;
 
-  test "blit from bytes" begin fun () ->
-    let bytes1 = Bytes.of_string "abc" in
-    let str2 = "abcdef" in
-    let buf2 = Lwt_bytes.of_string str2 in
-    let () = Lwt_bytes.blit_from_bytes bytes1 0 buf2 3 3 in
-    let check = "abcabc" = Lwt_bytes.to_string buf2 in
-    Lwt.return check
-  end;
+    test "blit from bytes" begin fun () ->
+      let bytes1 = Bytes.of_string "abc" in
+      let str2 = "abcdef" in
+      let buf2 = Lwt_bytes.of_string str2 in
+      let () = Lwt_bytes.blit_from_bytes bytes1 0 buf2 3 3 in
+      let check = "abcabc" = Lwt_bytes.to_string buf2 in
+      Lwt.return check
+    end;
 
-  test "blit from bytes out of bounds" begin fun () ->
-    let bytes1 = Bytes.of_string "abc" in
-    let str2 = "abcdef" in
-    let buf2 = Lwt_bytes.of_string str2 in
-    try
-      let () = Lwt_bytes.blit_from_bytes bytes1 150 buf2 150 150 in
-      Lwt.return_false
-    with
-    | Invalid_argument _ -> Lwt.return_true
-    | _ -> Lwt.return_false
-  end;
+    test "blit from bytes out of bounds" begin fun () ->
+      let bytes1 = Bytes.of_string "abc" in
+      let str2 = "abcdef" in
+      let buf2 = Lwt_bytes.of_string str2 in
+      try
+        let () = Lwt_bytes.blit_from_bytes bytes1 150 buf2 150 150 in
+        Lwt.return_false
+      with
+      | Invalid_argument _ -> Lwt.return_true
+      | _ -> Lwt.return_false
+    end;
 
-  test "blit to bytes" begin fun () ->
-    let str1 = "abc" in
-    let buf1 = Lwt_bytes.of_string str1 in
-    let str2 = "abcdef" in
-    let bytes2 = Bytes.of_string str2 in
-    let () = Lwt_bytes.blit_to_bytes buf1 0 bytes2 3 3 in
-    let check = "abcabc" = Bytes.to_string bytes2 in
-    Lwt.return check
-  end;
+    test "blit to bytes" begin fun () ->
+      let str1 = "abc" in
+      let buf1 = Lwt_bytes.of_string str1 in
+      let str2 = "abcdef" in
+      let bytes2 = Bytes.of_string str2 in
+      let () = Lwt_bytes.blit_to_bytes buf1 0 bytes2 3 3 in
+      let check = "abcabc" = Bytes.to_string bytes2 in
+      Lwt.return check
+    end;
 
-  test "blit to bytes out of bounds" begin fun () ->
-    let str1 = "abc" in
-    let buf1 = Lwt_bytes.of_string str1 in
-    let str2 = "abcdef" in
-    let bytes2 = Bytes.of_string str2 in
-    try
-      let () = Lwt_bytes.blit_to_bytes buf1 150 bytes2 150 150 in
-      Lwt.return_false
-    with
-    | Invalid_argument _ -> Lwt.return_true
-    | _ -> Lwt.return_false
-  end;
+    test "blit to bytes out of bounds" begin fun () ->
+      let str1 = "abc" in
+      let buf1 = Lwt_bytes.of_string str1 in
+      let str2 = "abcdef" in
+      let bytes2 = Bytes.of_string str2 in
+      try
+        let () = Lwt_bytes.blit_to_bytes buf1 150 bytes2 150 150 in
+        Lwt.return_false
+      with
+      | Invalid_argument _ -> Lwt.return_true
+      | _ -> Lwt.return_false
+    end;
 
-  test "unsafe blit" begin fun () ->
-    let str1 = "abc" in
-    let buf1 = Lwt_bytes.of_string str1 in
-    let str2 = "abcdef" in
-    let buf2 = Lwt_bytes.of_string str2 in
-    let () = Lwt_bytes.unsafe_blit buf1 0 buf2 3 3 in
-    let check = "abcabc" = Lwt_bytes.to_string buf2 in
-    Lwt.return check
-  end;
+    test "unsafe blit" begin fun () ->
+      let str1 = "abc" in
+      let buf1 = Lwt_bytes.of_string str1 in
+      let str2 = "abcdef" in
+      let buf2 = Lwt_bytes.of_string str2 in
+      let () = Lwt_bytes.unsafe_blit buf1 0 buf2 3 3 in
+      let check = "abcabc" = Lwt_bytes.to_string buf2 in
+      Lwt.return check
+    end;
 
-  test "unsafe blit from bytes" begin fun () ->
-    let bytes1 = Bytes.of_string "abc" in
-    let str2 = "abcdef" in
-    let buf2 = Lwt_bytes.of_string str2 in
-    let () = Lwt_bytes.unsafe_blit_from_bytes bytes1 0 buf2 3 3 in
-    let check = "abcabc" = Lwt_bytes.to_string buf2 in
-    Lwt.return check
-  end;
+    test "unsafe blit from bytes" begin fun () ->
+      let bytes1 = Bytes.of_string "abc" in
+      let str2 = "abcdef" in
+      let buf2 = Lwt_bytes.of_string str2 in
+      let () = Lwt_bytes.unsafe_blit_from_bytes bytes1 0 buf2 3 3 in
+      let check = "abcabc" = Lwt_bytes.to_string buf2 in
+      Lwt.return check
+    end;
 
-  test "unsafe blit to bytes" begin fun () ->
-    let str1 = "abc" in
-    let buf1 = Lwt_bytes.of_string str1 in
-    let str2 = "abcdef" in
-    let bytes2 = Bytes.of_string str2 in
-    let () = Lwt_bytes.unsafe_blit_to_bytes buf1 0 bytes2 3 3 in
-    let check = "abcabc" = Bytes.to_string bytes2 in
-    Lwt.return check
-  end;
+    test "unsafe blit to bytes" begin fun () ->
+      let str1 = "abc" in
+      let buf1 = Lwt_bytes.of_string str1 in
+      let str2 = "abcdef" in
+      let bytes2 = Bytes.of_string str2 in
+      let () = Lwt_bytes.unsafe_blit_to_bytes buf1 0 bytes2 3 3 in
+      let check = "abcabc" = Bytes.to_string bytes2 in
+      Lwt.return check
+    end;
 
-  test "proxy" begin fun () ->
-    let str = "abcdef" in
-    let buf = Lwt_bytes.of_string str in
-    let buf' = Lwt_bytes.proxy buf 3 3 in
-    let check1 = "def" = Lwt_bytes.to_string buf' in
-    let () = Lwt_bytes.set buf 3 'a' in
-    let check2 = "aef" = Lwt_bytes.to_string buf' in
-    Lwt.return (check1 && check2)
-  end;
+    test "proxy" begin fun () ->
+      let str = "abcdef" in
+      let buf = Lwt_bytes.of_string str in
+      let buf' = Lwt_bytes.proxy buf 3 3 in
+      let check1 = "def" = Lwt_bytes.to_string buf' in
+      let () = Lwt_bytes.set buf 3 'a' in
+      let check2 = "aef" = Lwt_bytes.to_string buf' in
+      Lwt.return (check1 && check2)
+    end;
 
-test "extract" begin fun () ->
-    let str = "abcdef" in
-    let buf = Lwt_bytes.of_string str in
-    let buf' = Lwt_bytes.extract buf 3 3 in
-    let check = "def" = Lwt_bytes.to_string buf' in
-    Lwt.return check
-  end;
+    test "extract" begin fun () ->
+      let str = "abcdef" in
+      let buf = Lwt_bytes.of_string str in
+      let buf' = Lwt_bytes.extract buf 3 3 in
+      let check = "def" = Lwt_bytes.to_string buf' in
+      Lwt.return check
+    end;
 
-test "extract out of bounds" begin fun () ->
-    let str = "abcdef" in
-    let buf = Lwt_bytes.of_string str in
-    try
-      let _ = Lwt_bytes.extract buf 150 150 in
-      Lwt.return_false
-    with
-    | Invalid_argument _ -> Lwt.return_true
-    | _ -> Lwt.return_false
-  end;
+    test "extract out of bounds" begin fun () ->
+      let str = "abcdef" in
+      let buf = Lwt_bytes.of_string str in
+      try
+        let _ = Lwt_bytes.extract buf 150 150 in
+        Lwt.return_false
+      with
+      | Invalid_argument _ -> Lwt.return_true
+      | _ -> Lwt.return_false
+    end;
 
-test "copy" begin fun () ->
-    let str = "abc" in
-    let buf = Lwt_bytes.of_string str in
-    let buf' = Lwt_bytes.copy buf in
-    let check = str = Lwt_bytes.to_string buf' in
-    Lwt.return check
-  end;
+    test "copy" begin fun () ->
+      let str = "abc" in
+      let buf = Lwt_bytes.of_string str in
+      let buf' = Lwt_bytes.copy buf in
+      let check = str = Lwt_bytes.to_string buf' in
+      Lwt.return check
+    end;
 
-  test "fill" begin fun () ->
-    let str = "abcdef" in
-    let buf = Lwt_bytes.of_string str in
-    let () = Lwt_bytes.fill buf 3 3 'a' in
-    let check = "abcaaa" = Lwt_bytes.to_string buf in
-    Lwt.return check
-  end;
+    test "fill" begin fun () ->
+      let str = "abcdef" in
+      let buf = Lwt_bytes.of_string str in
+      let () = Lwt_bytes.fill buf 3 3 'a' in
+      let check = "abcaaa" = Lwt_bytes.to_string buf in
+      Lwt.return check
+    end;
 
-  test "fill out of bounds" begin fun () ->
-    let str = "abcdef" in
-    let buf = Lwt_bytes.of_string str in
-    try
-      let () = Lwt_bytes.fill buf 150 150 'a' in
-      Lwt.return_false
-    with
-    | Invalid_argument _ -> Lwt.return_true
-    | _ -> Lwt.return_false
-  end;
+    test "fill out of bounds" begin fun () ->
+      let str = "abcdef" in
+      let buf = Lwt_bytes.of_string str in
+      try
+        let () = Lwt_bytes.fill buf 150 150 'a' in
+        Lwt.return_false
+      with
+      | Invalid_argument _ -> Lwt.return_true
+      | _ -> Lwt.return_false
+    end;
 
-  test "unsafe fill" begin fun () ->
-    let str = "abcdef" in
-    let buf = Lwt_bytes.of_string str in
-    let () = Lwt_bytes.unsafe_fill buf 3 3 'a' in
-    let check = "abcaaa" = Lwt_bytes.to_string buf in
-    Lwt.return check
-  end;
-]
+    test "unsafe fill" begin fun () ->
+      let str = "abcdef" in
+      let buf = Lwt_bytes.of_string str in
+      let () = Lwt_bytes.unsafe_fill buf 3 3 'a' in
+      let check = "abcaaa" = Lwt_bytes.to_string buf in
+      Lwt.return check
+    end;
+  ]
