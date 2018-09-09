be1317732a78c38de843ed92a5f290f3ccbef722
Cédric Le Moigne <cedlemo@gmx.com>
2018-09-09 09:13:07 +0200

    Lwt_bytes test : add bytes_equal function for OCaml 4.02 compatibility

--- test/unix/test_lwt_bytes.ml
+++ test/unix/test_lwt_bytes.ml
@@ -3,6 +3,8 @@
 
 open Test
 
+let bytes_equal b1 b2 = b1 = b2
+
 let suite = suite "lwt_bytes" [
     test "create" begin fun () ->
       let len = 5 in
@@ -74,7 +76,7 @@ let suite = suite "lwt_bytes" [
       let bytes = Bytes.of_string "abc" in
       let buff = Lwt_bytes.of_bytes bytes in
       let bytes' = Lwt_bytes.to_bytes buff in
-      let check = Bytes.equal bytes bytes' in
+      let check = bytes_equal bytes bytes' in
       Lwt.return check
     end;
 
