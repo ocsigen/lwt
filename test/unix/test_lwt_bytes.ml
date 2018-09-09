319ed2d061664793677cb1aee0d85986e12349f3
Cédric Le Moigne <cedlemo@gmx.com>
2018-09-09 09:43:04 +0200

    Lwt_bytes test : add upper limit out of bound for source test for blit

--- test/unix/test_lwt_bytes.ml
+++ test/unix/test_lwt_bytes.ml
@@ -131,6 +131,19 @@ let suite = suite "lwt_bytes" [
       | _ -> Lwt.return false
     end;
 
+    test "blit source out of bound: upper limit" begin fun () ->
+      let str1 = "abc" in
+      let buf1 = Lwt_bytes.of_string str1 in
+      let str2 = "abcdef" in
+      let buf2 = Lwt_bytes.of_string str2 in
+      try
+        let() = Lwt_bytes.blit buf1 150 buf2 3 3 in
+        Lwt.return_false
+      with
+      | Invalid_argument _ -> Lwt.return true
+      | _ -> Lwt.return false
+    end;
+
     test "blit from bytes" begin fun () ->
       let bytes1 = Bytes.of_string "abc" in
       let str2 = "abcdef" in
