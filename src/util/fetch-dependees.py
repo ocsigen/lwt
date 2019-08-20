72795299106962358c180d73e91c135e4936be46
Anton Bachin <antonbachin@yahoo.com>
2019-08-20 18:02:56 +0300

    fetch-dependees.py: avoid opam source
    [skip ci]

--- src/util/fetch-dependees.py
+++ src/util/fetch-dependees.py
@@ -33,6 +33,27 @@ def main():
     for package in packages:
         directory = os.path.join(DEPENDEES, package)
         remove_command = ["rm", "-rf", directory]
+
+        use_opam_source = False
+
+        info_command = ["opam", "info", "--field=dev-repo:", package]
+        try:
+            field = subprocess.check_output(info_command)
+            no_quotes = field[1:-2]
+            if no_quotes[0:9] != "git+https":
+                use_opam_source = True
+            else:
+                repo = no_quotes[4:]
+                clone_command = \
+                    ["git", "clone", "-q", "--depth", "1", repo, directory]
+                print "[%s]" % package, " ".join(clone_command)
+                subprocess.check_call(clone_command)
+        except:
+            use_opam_source = True
+
+        if not use_opam_source:
+            continue
+
         source_command = ["opam", "source", "--dir=" + directory]
         subprocess.check_call(remove_command)
         try:
