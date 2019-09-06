2a220b53f14672b57a11424d15d4779a91df86c2
Anton Bachin <antonbachin@yahoo.com>
2019-09-06 10:07:26 +0300

    AppVeyor: cache _build
    [skip travis]

--- src/util/appveyor-build.sh
+++ src/util/appveyor-build.sh
@@ -3,5 +3,16 @@ set -x
 
 eval `opam config env`
 
+if [ -d _cache/_build ]
+then
+    cp -r _cache/_build .
+fi
+
 make build
-make test
+dune runtest -j 1 --no-buffer --force
+
+if [ ! -d _cache/_build ]
+then
+    mkdir -p _cache
+    cp -r _build _cache
+fi
