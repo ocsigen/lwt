d767879dc9b731399f77ed17788d924b777dac69
Anton Bachin <antonbachin@yahoo.com>
2019-09-06 10:20:04 +0300

    AppVeyor: show timestamps
    [skip travis]

--- src/util/appveyor-build.sh
+++ src/util/appveyor-build.sh
@@ -3,16 +3,25 @@ set -x
 
 eval `opam config env`
 
+date
+
 if [ -d _cache/_build ]
 then
     cp -r _cache/_build .
 fi
 
 make build
+
+date
+
 dune runtest -j 1 --no-buffer --force
+
+date
 
 if [ ! -d _cache/_build ]
 then
     mkdir -p _cache
     cp -r _build _cache
 fi
+
+date
--- src/util/appveyor-install.sh
+++ src/util/appveyor-install.sh
@@ -1,7 +1,11 @@
 set -e
 set -x
 
+date
+
 opam init default https://github.com/fdopen/opam-repository-mingw.git#opam2 -c ocaml-variants.4.07.1+mingw64c --disable-sandboxing --yes --auto-setup
+
+date
 
 make dev-deps
 opam clean
