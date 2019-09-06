055c4658ad7f2557d96076aa56289a51c4384e21
Anton Bachin <antonbachin@yahoo.com>
2019-09-06 10:32:10 +0300

    AppVeyor: invalidate cache in cron builds
    [skip travis]

--- src/util/appveyor-install.sh
+++ src/util/appveyor-install.sh
@@ -1,6 +1,12 @@
 set -e
 set -x
 
+if [ "$APPVEYOR_SCHEDULED_BUILD" == True ]
+then
+    rm -rf ~/.opam
+    rm -rf ./_cache
+fi
+
 date
 
 opam init default https://github.com/fdopen/opam-repository-mingw.git#opam2 -c ocaml-variants.4.07.1+mingw64c --disable-sandboxing --yes --auto-setup
