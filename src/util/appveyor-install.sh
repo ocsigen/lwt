21eb0b9e31f8f98878b4f00f7575aa7876e3015b
Anton Bachin <antonbachin@yahoo.com>
2019-09-06 09:30:39 +0300

    AppVeyor: tar caching workaround no longer needed
    [skip travis]

--- src/util/appveyor-install.sh
+++ src/util/appveyor-install.sh
@@ -1,23 +1,10 @@
 set -e
 set -x
 
-DIRECTORY=`pwd`
+opam init default https://github.com/fdopen/opam-repository-mingw.git#opam2 -c ocaml-variants.4.07.1+mingw64c --disable-sandboxing --yes --auto-setup
 
-# AppVeyor does not cache empty subdirectories of .opam, such as $SWITCH/build.
-# To get around that, create a tar archive of .opam.
-CACHE=$DIRECTORY/opam-cache.tar
-
-if [ -f $CACHE ]
-then
-    ( cd ~ ; tar xf $CACHE )
-else
-    opam init default https://github.com/fdopen/opam-repository-mingw.git#opam2 -c ocaml-variants.4.07.1+mingw64c --disable-sandboxing --yes --auto-setup
-
-    make dev-deps
-    opam clean
-
-    ( cd ~ ; tar cf $CACHE .opam )
-fi
+make dev-deps
+opam clean
 
 eval `opam config env`
 
