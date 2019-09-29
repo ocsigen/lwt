fb00be1620197aa9c31f267ede87c678fffaeac8
Anton Bachin <antonbachin@yahoo.com>
2019-09-29 07:04:59 -0500

    Instrument flaky Lwt_timeout test

--- test/unix/test_lwt_timeout.ml
+++ test/unix/test_lwt_timeout.ml
@@ -238,6 +238,9 @@ let suite = suite "Lwt_timeout" [
 
     p1 >>= fun delta1 ->
     p2 >>= fun delta2 ->
+    (* Instrumentation for an occasionally failing test. *)
+    if not (delta1 >= 1. && delta1 < 2.1 && delta2 >= 1. && delta2 < 2.1) then
+      Printf.eprintf "\n%f %f\n" delta1 delta2;
     Lwt.return (delta1 >= 1. && delta1 < 2.1 && delta2 >= 1. && delta2 < 2.1)
   end;
 
