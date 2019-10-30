fea7ff2e59bc0669834ae7bed285c62f76f189ec
Anton Bachin <antonbachin@yahoo.com>
2019-10-30 10:24:31 +0300

    Retain buffer during Lwt_bytes.wait_mincore

--- src/unix/unix_c/unix_wait_mincore_job.c
+++ src/unix/unix_c/unix_wait_mincore_job.c
@@ -8,6 +8,7 @@
 #if !defined(LWT_ON_WINDOWS)
 
 #include <caml/bigarray.h>
+#include <caml/memory.h>
 #include <caml/mlvalues.h>
 #include <caml/unixsupport.h>
 #include <string.h>
@@ -19,6 +20,7 @@ LWT_NOT_AVAILABLE2(unix_wait_mincore_job)
 #else
 struct job_wait_mincore {
     struct lwt_unix_job job;
+    value ocaml_buffer;
     char *ptr;
 };
 
@@ -31,6 +33,7 @@ static void worker_wait_mincore(struct job_wait_mincore *job)
 
 static value result_wait_mincore(struct job_wait_mincore *job)
 {
+    caml_remove_generational_global_root(&job->ocaml_buffer);
     lwt_unix_free_job(&job->job);
     return Val_unit;
 }
@@ -39,6 +42,8 @@ CAMLprim value lwt_unix_wait_mincore_job(value val_buffer, value val_offset)
 {
     LWT_UNIX_INIT_JOB(job, wait_mincore, 0);
     job->ptr = (char *)Caml_ba_data_val(val_buffer) + Long_val(val_offset);
+    job->ocaml_buffer = val_buffer;
+    caml_register_generational_global_root(&job->ocaml_buffer);
     return lwt_unix_alloc_job(&(job->job));
 }
 #endif
