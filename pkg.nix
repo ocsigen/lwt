3fcce2f87b264f18ca96babe69cabd84d72764a2
David Johnson <djohnson.m@gmail.com>
2020-05-01 16:19:27 -0400

    Add mmap and ocplib-bigendian.

--- pkg.nix
+++ pkg.nix
@@ -1,12 +1,19 @@
-{ stdenv, buildDunePackage, result, cppo }:
+{ lib, fetchzip, pkgconfig, ncurses
+, libev, buildDunePackage, ocaml
+, cppo, ocplib-endian, result
+, mmap, seq, stdenv
+}:
 
-buildDunePackage rec {
-  pname = "lwt";
-  version = "0.5.0";
-  minimumOCamlVersion = "4.03";
+buildDunePackage {
   src = ./.;
-  propagatedBuildInputs = [ result cppo ];
-  doCheck = !stdenv.isDarwin;
+  pname = "lwt";
+  version = "5.0.0";
+  minimumOCamlVersion = "4.02";
+  buildInputs =
+    [ cppo ] ++ lib.optional (!lib.versionAtLeast ocaml.version "4.07") ncurses;
+  propagatedBuildInputs =
+    [ libev mmap ocplib-endian seq result ];
+  doCheck = true;
   meta = {
     homepage = https://ocsigen.org/lwt;
     description = "OCaml promises and concurrent I/O";
