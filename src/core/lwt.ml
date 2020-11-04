8aa565b6b1550aff2d1f47ece7591d97d2e7b672
Simon Cruanes <simon.cruanes.2007@m4x.org>
2020-11-04 00:23:41 -0500

    optimize `Lwt.both` to take less memory

--- src/core/lwt.ml
+++ src/core/lwt.ml
@@ -2585,13 +2585,19 @@ struct
 
     attach_callback_or_resolve_immediately ps
 
+  (* this is 3 words, smaller than the 2 times 2 words a pair of references
+     would take. *)
+  type ('a,'b) pair = {
+    mutable x1: 'a option;
+    mutable x2: 'b option;
+  }
+
   let both p1 p2 =
-    let v1 = ref None in
-    let v2 = ref None in
-    let p1' = bind p1 (fun v -> v1 := Some v; return_unit) in
-    let p2' = bind p2 (fun v -> v2 := Some v; return_unit) in
+    let pair = {x1=None; x2=None} in
+    let p1' = bind p1 (fun v -> pair.x1 <- Some v; return_unit) in
+    let p2' = bind p2 (fun v -> pair.x2 <- Some v; return_unit) in
     join [p1'; p2'] |> map (fun () ->
-      match !v1, !v2 with
+      match pair.x1, pair.x2 with
       | Some v1, Some v2 -> v1, v2
       | _ -> assert false)
 
