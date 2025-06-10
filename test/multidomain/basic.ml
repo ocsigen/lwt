802a58a0338a3a19d0ecd1f4151773e731b27c80
Raphaël Proust <code@bnwr.net>
2025-06-10 11:09:12 +0200

    tests: terser output, relevant exit codes

--- test/multidomain/basic.ml
+++ test/multidomain/basic.ml
@@ -4,53 +4,50 @@ open Lwt.Syntax
 let () = Lwt_unix.init_domain ()
 
 let p_one, w_one = Lwt.wait ()
+let v_one = 3
 let p_two, w_two = Lwt.wait ()
+let v_two = 2
 
-let d_one = Domain.spawn (fun () ->
+let d_mult = Domain.spawn (fun () ->
   (* domain one: wait for value from domain two then work and then send a value *)
   Lwt_main.run (
     let* () = Lwt_unix.sleep 0.01 in
     let* v_two = p_two in
-    Printf.printf "d%d  received %d\n" (Domain.self () :> int) v_two;
-    flush_all ();
+(*     Printf.printf "d%d  received %d\n" (Domain.self () :> int) v_two; *)
     let* () = Lwt_unix.sleep 0.1 in
-    flush_all ();
-    let v_one = 3 in
     Lwt.wakeup w_one v_one;
-    Printf.printf "d%d  sent %d\n" (Domain.self () :> int) v_one;
-    flush_all ();
-    let* v_two = p_two and* v_one = p_one in
+(*     Printf.printf "d%d  sent %d\n" (Domain.self () :> int) v_one; *)
     Lwt.return (v_two * v_one)
   )
 )
-let d_two = Domain.spawn (fun () ->
+let d_sum = Domain.spawn (fun () ->
   Lwt_main.run (
     let () =
       (* concurrent thread within domain "two" send a value and then work and
          then wait for a value from domain one *)
       Lwt.dont_wait (fun () ->
         let* () = Lwt_unix.sleep 0.1 in
-        Printf.printf "d%d  slept\n" (Domain.self () :> int);
-        flush_all ();
-        let v_two = 2 in
+(*         Printf.printf "d%d  slept\n" (Domain.self () :> int); *)
         Lwt.wakeup w_two v_two;
-        Printf.printf "d%d  sent %d\n" (Domain.self () :> int) v_two;
-        flush_all ();
-        let* from_one = p_one in
-        Printf.printf "d%d  received %d\n" (Domain.self () :> int) from_one;
-        flush_all ();
+(*         Printf.printf "d%d  sent %d\n" (Domain.self () :> int) v_two; *)
         Lwt.return ()
       )
       (fun _ -> exit 1)
     in
-    let* v_two = p_two and* v_one = p_one in
+    let* v_one = p_one in
     Lwt.return (v_two + v_one)
   )
 )
 
 
-let one = Domain.join d_one
-let two = Domain.join d_two
+let mult = Domain.join d_mult
+let sum = Domain.join d_sum
 
-let () = Printf.printf "product: %d, sum: %d\n" one two
-let () = flush_all ()
+let () =
+  if mult = v_one * v_two && sum = v_one + v_two then begin
+    Printf.printf "basic: ✓\n";
+    exit 0
+  end else begin
+    Printf.printf "basic: ×\n";
+    exit 1
+  end
--- test/multidomain/movingpromises.ml
+++ test/multidomain/movingpromises.ml
@@ -4,16 +4,16 @@ let rec worker ongoing_tasks recv_task f =
   let* task = Lwt_stream.get recv_task in
   match task with
   | None ->
-      let () = Printf.printf "worker(%d) received interrupt\n" (Domain.self () :> int); flush_all() in
+(*       let () = Printf.printf "worker(%d) received interrupt\n" (Domain.self () :> int); flush_all() in *)
       Lwt.join ongoing_tasks
-  | Some (idx, data, resolver) ->
+  | Some (_idx, data, resolver) ->
       let task =
-        let () = Printf.printf "worker(%d) received task(%d)\n" (Domain.self () :> int) idx; flush_all() in
+(*         let () = Printf.printf "worker(%d) received task(%d)\n" (Domain.self () :> int) _idx; flush_all() in *)
         let* data in
-        let () = Printf.printf "worker(%d) received task(%d) data(%S)\n" (Domain.self () :> int) idx data; flush_all() in
+(*         let () = Printf.printf "worker(%d) received task(%d) data(%S)\n" (Domain.self () :> int) _idx data; flush_all() in *)
         let* result = f data in
         Lwt.wakeup resolver result;
-        let () = Printf.printf "worker(%d) sent result(%d) for task(%d)\n" (Domain.self () :> int) result idx; flush_all() in
+(*         let () = Printf.printf "worker(%d) sent result(%d) for task(%d)\n" (Domain.self () :> int) result _idx; flush_all() in *)
         Lwt.return ()
       in
       let* () = Lwt.pause () in
@@ -41,6 +41,9 @@ let simulate_input data =
   let* () = Lwt_unix.sleep (0.01 *. float_of_int simulated_work_duration) in
   Lwt.return data
 
+let input = [""; "adsf"; "lkjh"; "lkjahsdflkjahdlfkjha"; "0"; ""; ""; ""; ""; ""; "adf"; "ASDSKJLHDAS"; "WPOQIEU"; "DSFALKHJ"; ""; ""; ""; ""; "SD"; "SD"; "SAD; SD;SD"; "ad"; "...."]
+let expected_result = input |> List.map String.length |> List.map string_of_int |> String.concat ","
+
 let main () =
   let send_task1, dw1 = spawn_domain_worker simulate_work in
   let send_task2, dw2 = spawn_domain_worker simulate_work in
@@ -67,8 +70,12 @@ let main () =
   in
   let () = Domain.join dw1 in
   let () = Domain.join dw2 in
-  Printf.printf "lengths: %s\n" l;
-  flush_all ();
-  exit 0
+  if l = expected_result then begin
+    Printf.printf "moving-promises: ✓\n";
+    exit 0
+  end else begin
+    Printf.printf "moving-promises: ×\n";
+    exit 1
+  end
 
 let () = main ()
