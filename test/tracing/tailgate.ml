91eb8eb8eea53938fbe5a0b6b6607d5a22a0f7a5
Raphaël Proust <code@bnwr.net>
2025-10-17 08:39:11 +0800

    tail tracing demo: emit fuchsia trace

--- test/tracing/tailgate.ml
+++ test/tracing/tailgate.ml
@@ -19,8 +19,14 @@ let cursor =
 
 let (_, _) = Unix.waitpid [WUNTRACED] pid
 
-let () = Printf.printf "crash!\n"
+let () = Printf.printf "crash! writing trace file %s/%d.tail\n" tmpdir pid
 let () =
+  let buf_pool = Trace_fuchsia.Buf_pool.create () in
+  let buf = Trace_fuchsia.Buf_chain.create ~sharded:false ~buf_pool () in
+  let oc = open_out_bin (Printf.sprintf "%s/%d.tail" tmpdir pid) in
+  let { Trace_fuchsia.Exporter.write_bufs; flush; close } as exporter = Trace_fuchsia.Exporter.of_out_channel ~close_channel:true oc in
+  let subscriber = Trace_fuchsia.Subscriber.create ~buf_pool ~pid:0 ~exporter () in
+  Trace_fuchsia.Subscriber.Callbacks.on_init subscriber ~time_ns:0L;
   let cb =
     Runtime_events.Callbacks.create ()
     |> Runtime_events.Callbacks.add_user_event
@@ -28,11 +34,16 @@ let () =
         (fun _ t u x ->
           match Runtime_events.User.tag u with
           | Lwt_runtime_events.Trace ->
-              Printf.printf "[%Ld] %s\n" (Runtime_events.Timestamp.to_int64 t) x
+              Trace_fuchsia.Writer.Event.Instant.encode buf ~name:"bind"
+              ~t_ref:(Ref 1) ~time_ns:(Runtime_events.Timestamp.to_int64 t) ~args:["location",A_string x] ()
           | _ -> ())
   in
   let _ : int = Runtime_events.read_poll cursor cb None in
+  Trace_fuchsia.Buf_chain.ready_all_non_empty buf;
+  Trace_fuchsia.Buf_chain.pop_ready ~f:write_bufs buf;
+  flush ();
+  Trace_fuchsia.Subscriber.close subscriber;
+  close ();
   ()
 
-let () = flush stdout
 let () = exit 0
